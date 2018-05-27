881cd2f95566229a9e2fb7e2e4e6eb1efa19fdcc
parzivail <colby.newman2000@gmail.com>
2018-05-27 12:34:22 -0400

    simplified hook

--- src/main/java/com/parzivail/swg/gui/GuiScreenTrailer.java
+++ src/main/java/com/parzivail/swg/gui/GuiScreenTrailer.java
@@ -1,5 +1,7 @@
 package com.parzivail.swg.gui;
 
+import com.parzivail.util.ui.Fx;
+import com.parzivail.util.ui.gltk.GL;
 import net.minecraft.client.gui.GuiScreen;
 
 public class GuiScreenTrailer extends GuiScreen
@@ -21,6 +23,9 @@ public class GuiScreenTrailer extends GuiScreen
 	public void drawScreen(int mouseX, int mouseY, float partialTicks)
 	{
 		super.drawScreen(mouseX, mouseY, partialTicks);
+
+		GL.Color(0f, 0f, 0f);
+		Fx.D2.DrawSolidRectangle(0, 0, width, height);
 
 		this.fontRendererObj.drawString("Hello, World!", 10, 10, 0xFFFFFF);
 	}
