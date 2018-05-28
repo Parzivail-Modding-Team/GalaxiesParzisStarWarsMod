9d6bdd4f998803c0be32c7293abeede69928e0dc
parzivail <colby.newman2000@gmail.com>
2018-05-27 20:38:01 -0400

    added stars

--- src/main/java/com/parzivail/swg/gui/GuiScreenTrailer.java
+++ src/main/java/com/parzivail/swg/gui/GuiScreenTrailer.java
@@ -7,8 +7,10 @@ import com.parzivail.util.ui.Fx;
 import com.parzivail.util.ui.GLPalette;
 import com.parzivail.util.ui.Timeline;
 import com.parzivail.util.ui.TimelineEvent;
+import com.parzivail.util.ui.gltk.AttribMask;
 import com.parzivail.util.ui.gltk.EnableCap;
 import com.parzivail.util.ui.gltk.GL;
+import com.parzivail.util.ui.gltk.PrimitiveType;
 import net.minecraft.client.audio.ISound;
 import net.minecraft.client.audio.PositionedSoundRecord;
 import net.minecraft.client.gui.GuiScreen;
@@ -16,22 +18,47 @@ import net.minecraft.util.ResourceLocation;
 import org.lwjgl.input.Keyboard;
 import org.lwjgl.opengl.GL11;
 import org.lwjgl.util.glu.GLU;
+import org.lwjgl.util.vector.Vector2f;
 
 import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
 
 public class GuiScreenTrailer extends GuiScreen
 {
 	public static boolean hasBeenShown = false;
+	private static List<Star> stars = new ArrayList<>();
+
+	static
+	{
+		for (int i = 0; i < 5000; i++)
+		{
+			Random r = new Random();
+
+			float x = r.nextFloat();
+			float y = r.nextFloat();
+			int c = (int)(r.nextFloat() * 255);
+
+			float s = 1;//(float)Math.sqrt(Math.pow(x - 0.5, 2) + Math.pow(y - 0.5, 2));
+
+			float dX = (x - 0.5f) / s;
+			float dY = (y - 0.5f) / s;
+
+			stars.add(new Star(new Vector2f(x, y), new Vector2f(dX, dY), r.nextFloat() * 1.5f + 0.5f, Fx.Util.GetRgb(c, c, c)));
+		}
+	}
 
 	private Timeline timeline;
 	private ISound titleMusic;
-	private TrailerPhase phase;
+	private TrailerPhase phase = TrailerPhase.LONGTIMEAGO;
 
 	private ResourceLocation brandLogo = Resources.location("textures/brand/logo.png");
 
 	private AnimatedValue longTimeAgoOpacity = new AnimatedValue(0, 1000);
 	private AnimatedValue logoScale = new AnimatedValue(1, 10500);
-	private AnimatedValue crawlTranslate = new AnimatedValue(0, 70000);
+	private AnimatedValue crawlTranslate = new AnimatedValue(0, 75000);
+	private AnimatedValue crawlOpacity = new AnimatedValue(1, 2000);
+	private AnimatedValue panTranslate = new AnimatedValue(0, 5000);
 
 	public GuiScreenTrailer()
 	{
@@ -42,11 +69,10 @@ public class GuiScreenTrailer extends GuiScreen
 
 		ArrayList<TimelineEvent> events = new ArrayList<>();
 		events.add(new TimelineEvent(0, (t) -> {
-			this.phase = TrailerPhase.LONGTIMEAGO;
 			this.longTimeAgoOpacity.queueAnimatingTo(1);
 		}));
 		events.add(new TimelineEvent(4000, (t) -> {
-			longTimeAgoOpacity.queueAnimatingTo(0);
+			this.longTimeAgoOpacity.queueAnimatingTo(0);
 		}));
 		events.add(new TimelineEvent(5000, (t) -> {
 			this.phase = TrailerPhase.STARWARS;
@@ -57,8 +83,15 @@ public class GuiScreenTrailer extends GuiScreen
 			this.phase = TrailerPhase.CRAWL;
 			this.crawlTranslate.animateTo(500);
 		}));
-		events.add(new TimelineEvent(85500, (t) -> {
-			close();
+		events.add(new TimelineEvent(88500, (t) -> {
+			this.crawlOpacity.queueAnimatingTo(0);
+		}));
+		events.add(new TimelineEvent(90500, (t) -> {
+			this.phase = TrailerPhase.PAN;
+			this.panTranslate.queueAnimatingTo(1);
+		}));
+		events.add(new TimelineEvent(98000, (t) -> {
+			this.close();
 		}));
 		timeline = new Timeline(events);
 		timeline.start();
@@ -112,55 +145,74 @@ public class GuiScreenTrailer extends GuiScreen
 			this.fontRendererObj.drawString(s, xPos, yPos, color);
 			this.fontRendererObj.drawString(s2, xPos, yPos + this.fontRendererObj.FONT_HEIGHT, color);
 		}
-		else if (phase == TrailerPhase.STARWARS)
+		else
 		{
-			GL.Color(GLPalette.WHITE);
+			GL.PushAttrib(AttribMask.EnableBit);
+			GL.Disable(EnableCap.Texture2D);
 			GL.Enable(EnableCap.Blend);
-			GL11.glBlendFunc(GL11.GL_SRC_ALPHA, GL11.GL_ONE_MINUS_SRC_ALPHA);
-			Client.mc.renderEngine.bindTexture(brandLogo);
-			GL.Translate(this.width / 2, this.height / 2, 0);
-			GL.Scale(logoScale.getValue());
-			GL.Translate(-150, -150, 0);
-			Fx.D2.DrawSolidRectangle(0, 0, 300, 300);
-		}
-		else if (phase == TrailerPhase.CRAWL)
-		{
-			GL11.glMatrixMode(GL11.GL_PROJECTION);
-			GL11.glLoadIdentity();
-			GLU.gluPerspective(70, width / (float)height, 0.1f, 1000);
-			GL11.glMatrixMode(GL11.GL_MODELVIEW);
-			GL11.glLoadIdentity();
+			GL.Enable(EnableCap.PointSmooth);
+			GL11.glHint(GL11.GL_POINT_SMOOTH_HINT, GL11.GL_NICEST);
+			for (Star s : stars)
+			{
+				GL11.glPointSize(s.size * width / (float)1920);
+				GL.Color(s.color, 128);
 
-			int w = this.fontRendererObj.getStringWidth("spies managed to steal secret");
-			int i = 0;
+				GL.Begin(PrimitiveType.Points);
+				GL.Vertex2(s.pos.x * (float)width, s.pos.y * (float)height);
+				GL.End();
+			}
+			GL.PopAttrib();
 
-			GL.Scale(1, -1, -1);
-			GL.Translate(-w / 2f, 0, 100);
-			GL.Rotate(-55, 1, 0, 0);
+			if (phase == TrailerPhase.STARWARS)
+			{
+				GL.Color(GLPalette.WHITE);
+				GL.Enable(EnableCap.Blend);
+				GL11.glBlendFunc(GL11.GL_SRC_ALPHA, GL11.GL_ONE_MINUS_SRC_ALPHA);
+				Client.mc.renderEngine.bindTexture(brandLogo);
+				GL.Translate(this.width / 2, this.height / 2, 0);
+				GL.Scale(logoScale.getValue());
+				GL.Translate(-150, -150, 0);
+				Fx.D2.DrawSolidRectangle(0, 0, 300, 300);
+			}
+			else if (phase == TrailerPhase.CRAWL)
+			{
+				GL11.glMatrixMode(GL11.GL_PROJECTION);
+				GL11.glLoadIdentity();
+				GLU.gluPerspective(70, width / (float)height, 0.1f, 1000);
+				GL11.glMatrixMode(GL11.GL_MODELVIEW);
+				GL11.glLoadIdentity();
 
-			GL.Translate(0, 65 - crawlTranslate.getValue(), 0);
+				int w = this.fontRendererObj.getStringWidth("spies managed to steal secret");
+				int i = 0;
 
-			drawJustifiedLine("It is a period of civil war.", w, i++, true);
-			drawJustifiedLine("Rebel spaceships, striking", w, i++, true);
-			drawJustifiedLine("from a hidden base, have won", w, i++, true);
-			drawJustifiedLine("their first victory against", w, i++, true);
-			drawJustifiedLine("the evil Galactic Empire.", w, i++, false);
-			i++;
-			drawJustifiedLine("During the battle, Rebel", w, i++, true);
-			drawJustifiedLine("spies managed to steal secret", w, i++, true);
-			drawJustifiedLine("plans to the Empire's", w, i++, true);
-			drawJustifiedLine("ultimate weapon, the DEATH", w, i++, true);
-			drawJustifiedLine("STAR, an armored space", w, i++, true);
-			drawJustifiedLine("station with enough power to", w, i++, true);
-			drawJustifiedLine("destroy an entire planet.", w, i++, false);
-			i++;
-			drawJustifiedLine("Pursued by the Empire's", w, i++, true);
-			drawJustifiedLine("sinister agents, Princess", w, i++, true);
-			drawJustifiedLine("Leia races home aboard her", w, i++, true);
-			drawJustifiedLine("starship, custodian of the", w, i++, true);
-			drawJustifiedLine("stolen plans that can save", w, i++, true);
-			drawJustifiedLine("her people and restore", w, i++, true);
-			drawJustifiedLine("freedom to the galaxy.....", w, i++, false);
+				GL.Scale(1, -1, -1);
+				GL.Translate(-w / 2f, 0, 100);
+				GL.Rotate(-55, 1, 0, 0);
+
+				GL.Translate(0, 65 - crawlTranslate.getValue(), 0);
+
+				drawJustifiedLine("It is a period of civil war.", w, i++, true);
+				drawJustifiedLine("Rebel spaceships, striking", w, i++, true);
+				drawJustifiedLine("from a hidden base, have won", w, i++, true);
+				drawJustifiedLine("their first victory against", w, i++, true);
+				drawJustifiedLine("the evil Galactic Empire.", w, i++, false);
+				i++;
+				drawJustifiedLine("During the battle, Rebel", w, i++, true);
+				drawJustifiedLine("spies managed to steal secret", w, i++, true);
+				drawJustifiedLine("plans to the Empire's", w, i++, true);
+				drawJustifiedLine("ultimate weapon, the DEATH", w, i++, true);
+				drawJustifiedLine("STAR, an armored space", w, i++, true);
+				drawJustifiedLine("station with enough power to", w, i++, true);
+				drawJustifiedLine("destroy an entire planet.", w, i++, false);
+				i++;
+				drawJustifiedLine("Pursued by the Empire's", w, i++, true);
+				drawJustifiedLine("sinister agents, Princess", w, i++, true);
+				drawJustifiedLine("Leia races home aboard her", w, i++, true);
+				drawJustifiedLine("starship, custodian of the", w, i++, true);
+				drawJustifiedLine("stolen plans that can save", w, i++, true);
+				drawJustifiedLine("her people and restore", w, i++, true);
+				drawJustifiedLine("freedom to the galaxy.....", w, i++, false);
+			}
 		}
 
 		GL.PopMatrix();
@@ -169,10 +221,12 @@ public class GuiScreenTrailer extends GuiScreen
 	private void drawJustifiedLine(String line, int width, int yOffset, boolean justify)
 	{
 		int y = (int)(yOffset * this.fontRendererObj.FONT_HEIGHT * 1.25f);
+		float lerp = crawlOpacity.getValue() * 255;
+		int color = Fx.Util.GetRgb((int)(lerp * 255f / 255), (int)(lerp * 212f / 255), (int)(lerp * 0f / 255));
 
 		if (!justify)
 		{
-			this.fontRendererObj.drawString(line, 0, y, GLPalette.SW_YELLOW);
+			this.fontRendererObj.drawString(line, 0, y, color);
 			return;
 		}
 
@@ -183,8 +237,24 @@ public class GuiScreenTrailer extends GuiScreen
 		for (char c : characters)
 		{
 			int charWidth = this.fontRendererObj.getCharWidth(c);
-			this.fontRendererObj.drawString(String.valueOf(c), (int)x, y, GLPalette.SW_YELLOW);
+			this.fontRendererObj.drawString(String.valueOf(c), (int)x, y, color);
 			x += charWidth * scalar;
+		}
+	}
+
+	private static class Star
+	{
+		public final Vector2f pos;
+		public final Vector2f dir;
+		public final float size;
+		public final int color;
+
+		public Star(Vector2f pos, Vector2f dir, float size, int color)
+		{
+			this.pos = pos;
+			this.dir = dir;
+			this.size = size;
+			this.color = color;
 		}
 	}
 
