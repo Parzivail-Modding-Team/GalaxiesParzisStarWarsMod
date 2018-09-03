93e1650864b879649c126f807f7a6760b3523b74
parzivail <colby.newman2000@gmail.com>
2018-09-02 21:43:19 -0400

    rough dialogue gui

--- src/main/java/com/parzivail/swg/gui/GuiDialogue.java
+++ src/main/java/com/parzivail/swg/gui/GuiDialogue.java
@@ -6,12 +6,9 @@ import com.parzivail.swg.gui.modern.ModernButton;
 import com.parzivail.swg.proxy.Client;
 import com.parzivail.swg.registry.QuestRegister;
 import com.parzivail.util.binary.ned.NedInteraction;
-import com.parzivail.util.binary.ned.NedNode;
 import com.parzivail.util.binary.ned.NodeType;
 import com.parzivail.util.common.AnimatedValue;
 import com.parzivail.util.common.TextUtils;
-import com.parzivail.util.ui.Timeline;
-import com.parzivail.util.ui.TimelineEvent;
 import com.parzivail.util.ui.gltk.AttribMask;
 import com.parzivail.util.ui.gltk.EnableCap;
 import com.parzivail.util.ui.gltk.GL;
@@ -21,10 +18,10 @@ import net.minecraft.entity.Entity;
 import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.entity.player.InventoryPlayer;
 import net.minecraftforge.common.DimensionManager;
+import org.lwjgl.input.Keyboard;
 import org.newdawn.slick.Color;
 import org.newdawn.slick.opengl.TextureImpl;
 
-import java.util.ArrayList;
 import java.util.EnumSet;
 
 public class GuiDialogue extends GuiContainer
@@ -32,7 +29,6 @@ public class GuiDialogue extends GuiContainer
 	private final Entity target;
 	private final EntityPlayer player;
 	private final NedInteraction interaction;
-	private final Timeline textFadeOut;
 	private AnimatedValue textFadeOutValue = new AnimatedValue(0, 500);
 
 	private ModernButton bOpt1;
@@ -40,7 +36,6 @@ public class GuiDialogue extends GuiContainer
 	private ModernButton bOpt3;
 	private ModernButton bOpt4;
 	private ModernArrowButton bNext;
-	private ModernButton bExit;
 	private final ModernButton[] options = new ModernButton[4];
 
 	private String npcDialogue = "";
@@ -56,28 +51,6 @@ public class GuiDialogue extends GuiContainer
 		player = (EntityPlayer)DimensionManager.getWorld(inventoryPlayer.player.dimension).getEntityByID(inventoryPlayer.player.getEntityId());
 
 		interaction = QuestRegister.complexQuest.createInteraction(player);
-
-		ArrayList<TimelineEvent> keyframes = new ArrayList<>();
-		keyframes.add(new TimelineEvent(0, timelineEvent -> {
-			bOpt1.visible = bOpt2.visible = bOpt3.visible = bOpt4.visible = false;
-			bNext.visible = false;
-
-			textFadeOutValue = new AnimatedValue(0, Math.min(npcDialogue.length() * 10, 500));
-			textFadeOutValue.queueAnimatingTo(1);
-		}));
-		keyframes.add(new TimelineEvent(500, timelineEvent -> showButtons()));
-		textFadeOut = new Timeline(keyframes);
-	}
-
-	private void showButtons()
-	{
-		if (interaction.getNextNode(0).type == NodeType.NpcDialogue)
-		{
-			bOpt1.visible = bOpt2.visible = bOpt3.visible = bOpt4.visible = false;
-			bNext.visible = true;
-		}
-		else
-			movePastNpcDialogue();
 	}
 
 	@Override
@@ -85,13 +58,12 @@ public class GuiDialogue extends GuiContainer
 	{
 		super.initGui();
 
-		options[0] = bOpt1 = new ModernButton(0, (width - 150) / 2, 25, 150, 10, "OPT1");
-		options[1] = bOpt2 = new ModernButton(1, (width - 150) / 2, 40, 150, 10, "OPT2");
-		options[2] = bOpt3 = new ModernButton(2, (width - 150) / 2, 55, 150, 10, "OPT3");
-		options[3] = bOpt4 = new ModernButton(3, (width - 150) / 2, 70, 150, 10, "OPT4");
+		options[0] = bOpt1 = new ModernButton(0, (width - 150) / 2, height - 70, 150, 10, "OPT1");
+		options[1] = bOpt2 = new ModernButton(1, (width - 150) / 2, height - 55, 150, 10, "OPT2");
+		options[2] = bOpt3 = new ModernButton(2, (width - 150) / 2, height - 40, 150, 10, "OPT3");
+		options[3] = bOpt4 = new ModernButton(3, (width - 150) / 2, height - 25, 150, 10, "OPT4");
 
-		bNext = new ModernArrowButton(4, (width - 5) / 2, 80);
-		bExit = new ModernButton(5, (width - 150) / 2, 95, 150, 10, "Exit");
+		bNext = new ModernArrowButton(4, (width - 150) / 2 + 160, height - 25);
 
 		buttonList.add(bOpt1);
 		buttonList.add(bOpt2);
@@ -99,7 +71,6 @@ public class GuiDialogue extends GuiContainer
 		buttonList.add(bOpt4);
 
 		buttonList.add(bNext);
-		buttonList.add(bExit);
 
 		bOpt1.visible = bOpt2.visible = bOpt3.visible = bOpt4.visible = false;
 		bNext.visible = false;
@@ -108,29 +79,37 @@ public class GuiDialogue extends GuiContainer
 	}
 
 	@Override
-	public void updateScreen()
-	{
-		super.updateScreen();
-
-		textFadeOut.tick();
-	}
-
-	@Override
 	public void drawScreen(int mouseX, int mouseY, float partialTicks)
 	{
 		super.drawScreen(mouseX, mouseY, partialTicks);
 
-		GL.PushAttrib(EnumSet.of(AttribMask.EnableBit, AttribMask.LineBit));
-		GL.Enable(EnableCap.Blend);
-		GL.Enable(EnableCap.Texture2D);
-		GL.PushMatrix();
-		int w = Client.brandonReg.getWidth(npcDialogue) / 4;
-		GL.Translate((width - w) / 2, 10, 0);
-		GL.Scale(0.25f);
-		TextureImpl.bindNone();
-		Client.brandonReg.drawString(0, 0, TextUtils.scrambleString(npcDialogue, textFadeOutValue.getValue()), Color.white);
-		GL.PopMatrix();
-		GL.PopAttrib();
+		if (interaction.node.type == NodeType.NpcDialogue)
+		{
+			GL.PushAttrib(EnumSet.of(AttribMask.EnableBit, AttribMask.LineBit));
+			GL.Enable(EnableCap.Blend);
+			GL.Enable(EnableCap.Texture2D);
+			GL.PushMatrix();
+			int w = Client.brandonReg.getWidth(npcDialogue) / 4;
+			GL.Translate((int)((width - w) / 2f), height - 42, 0);
+			GL.Scale(0.25f);
+			TextureImpl.bindNone();
+			Client.brandonReg.drawString(0, 0, TextUtils.scrambleString(npcDialogue, textFadeOutValue.getValue()), Color.white);
+			GL.PopMatrix();
+			GL.PopAttrib();
+		}
+	}
+
+	@Override
+	protected void keyTyped(char typedChar, int keyCode)
+	{
+		// do not allow player to exit until dialogue is done
+		if (keyCode != Keyboard.KEY_ESCAPE && keyCode != mc.gameSettings.keyBindInventory.getKeyCode())
+			super.keyTyped(typedChar, keyCode);
+		if (keyCode == Keyboard.KEY_RETURN || keyCode == Keyboard.KEY_SPACE)
+		{
+			if (interaction.node.type == NodeType.NpcDialogue)
+				movePastNpcDialogue();
+		}
 	}
 
 	@Override
@@ -146,11 +125,6 @@ public class GuiDialogue extends GuiContainer
 				break;
 			case 4:
 				movePastNpcDialogue();
-				break;
-			case 5:
-				mc.displayGuiScreen(null);
-				mc.setIngameFocus();
-				break;
 			default:
 				break;
 		}
@@ -167,30 +141,29 @@ public class GuiDialogue extends GuiContainer
 		if (interaction.node == null)
 			return;
 
-		bExit.visible = false;
-
 		if (interaction.node.type == NodeType.NpcDialogue)
 		{
 			npcDialogue = interaction.node.outputs.get(0).text;
-			textFadeOut.start();
+
+			bOpt1.visible = bOpt2.visible = bOpt3.visible = bOpt4.visible = false;
+			bNext.visible = true;
+
+			textFadeOutValue = new AnimatedValue(0, Math.min(npcDialogue.length() * 10, 500));
+			textFadeOutValue.queueAnimatingTo(1);
 		}
 		else if (interaction.node.type == NodeType.PlayerDialogue)
 		{
 			bOpt1.visible = bOpt2.visible = bOpt3.visible = bOpt4.visible = false;
 			bNext.visible = false;
-			NedNode nextNode = interaction.node;
 
-			for (int i = 0; i < nextNode.outputs.size(); i++)
+			for (int i = 0; i < interaction.node.outputs.size(); i++)
 			{
-				options[i].setText(nextNode.outputs.get(i).text);
+				options[i].setText(interaction.node.outputs.get(i).text);
 				options[i].visible = true;
 			}
 		}
 		else if (interaction.node.type == NodeType.End)
-		{
-			bOpt1.visible = bOpt2.visible = bOpt3.visible = bOpt4.visible = bNext.visible = false;
-			bExit.visible = true;
-		}
+			mc.thePlayer.closeScreen();
 	}
 
 	private void takeBranch(int id)
@@ -199,14 +172,13 @@ public class GuiDialogue extends GuiContainer
 		refreshDisplay();
 	}
 
+	public void drawWorldBackground(int tint)
+	{
+	}
+
 	@Override
 	protected void drawGuiContainerBackgroundLayer(float partialTicks, int mouseX, int mouseY)
 	{
-		//		ScaledResolution sr = Client.resolution;
-		//		GL.PushAttrib(AttribMask.EnableBit);
-		//		GL.Disable(EnableCap.Texture2D);
-		//		GL.Color(GLPalette.ALMOST_BLACK);
-		//		D2.DrawSolidRectangle(0, 0, sr.getScaledWidth(), sr.getScaledHeight());
-		//		GL.PopAttrib();
+		drawGradientRect(5, height - 75, width - 5, height - 5, 0xDD000000, 0xAA000000);
 	}
 }
