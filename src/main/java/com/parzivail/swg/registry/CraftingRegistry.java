ad358fc94ce17c9cb9d5725cbc2a4da4b6682966
parzivail <colby.newman2000@gmail.com>
2018-10-22 21:27:20 -0400

    add food cooking recipes

--- src/main/java/com/parzivail/swg/registry/CraftingRegistry.java
+++ src/main/java/com/parzivail/swg/registry/CraftingRegistry.java
@@ -1,9 +1,16 @@
 package com.parzivail.swg.registry;
 
+import cpw.mods.fml.common.registry.GameRegistry;
+import net.minecraft.item.ItemStack;
+
 public class CraftingRegistry
 {
 	public static void register()
 	{
-
+		GameRegistry.addSmelting(ItemRegister.banthaChop, new ItemStack(ItemRegister.banthaSteak, 1), 0.2F);
+		GameRegistry.addSmelting(ItemRegister.gizkaChop, new ItemStack(ItemRegister.gizkaSteak, 1), 0.2F);
+		GameRegistry.addSmelting(ItemRegister.nerfChop, new ItemStack(ItemRegister.nerfSteak, 1), 0.2F);
+		GameRegistry.addSmelting(ItemRegister.mynockWing, new ItemStack(ItemRegister.friedMynockWing, 1), 0.2F);
+		GameRegistry.addSmelting(ItemRegister.qrikkiBread, new ItemStack(ItemRegister.qrikkiWaffle, 1), 0.2F);
 	}
 }
