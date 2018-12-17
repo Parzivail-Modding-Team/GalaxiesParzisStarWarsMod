f3a312ff4b388f473fb06da84f97e7745883ee00
Kaeptm Blaubaer <kaeptmblaubaer1000@gmail.com>
2018-12-17 17:30:24 +0100

    Make the jar executable, it will show an error message if directly executed

---
+++ src/main/java/com/parzivail/swg/NotAGameMain.java
@@ -0,0 +1,9 @@
+package com.parzivail.swg;
+
+import javax.swing.*;
+
+public class NotAGameMain {
+    public static void main(String[] args) {
+        javax.swing.JOptionPane.showMessageDialog(null, "This .jar file is a Minecraft Forge mod for 1.7.10, not a program.", "Not a program", JOptionPane.ERROR_MESSAGE);
+    }
+}
