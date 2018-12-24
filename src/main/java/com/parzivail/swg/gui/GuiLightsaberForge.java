bb411507d562d066939b3559216bc6356b68eca8
parzivail <colby.newman2000@gmail.com>
2018-12-24 14:14:50 -0500

    added color picker to creative saber menu

--- src/main/java/com/parzivail/swg/gui/GuiLightsaberForge.java
+++ src/main/java/com/parzivail/swg/gui/GuiLightsaberForge.java
@@ -3,10 +3,16 @@ package com.parzivail.swg.gui;
 import com.parzivail.swg.Resources;
 import com.parzivail.swg.container.ContainerLightsaberForge;
 import com.parzivail.swg.item.lightsaber.LightsaberData;
+import com.parzivail.swg.item.lightsaber.LightsaberDescriptor;
 import com.parzivail.swg.network.TransactionBroker;
 import com.parzivail.swg.tile.TileLightsaberForge;
 import com.parzivail.swg.transaction.TransactionSetLightsaberDescriptor;
+import com.parzivail.util.ui.Fx;
+import com.parzivail.util.ui.GLPalette;
+import com.parzivail.util.ui.gltk.EnableCap;
+import com.parzivail.util.ui.gltk.GL;
 import cpw.mods.fml.client.config.GuiCheckBox;
+import cpw.mods.fml.client.config.GuiSlider;
 import net.minecraft.client.gui.GuiButton;
 import net.minecraft.client.gui.inventory.GuiContainer;
 import net.minecraft.client.resources.I18n;
@@ -24,6 +30,11 @@ public class GuiLightsaberForge extends GuiContainer
 	private LightsaberData lightsaberData;
 
 	private GuiCheckBox cbSaberUnstable;
+	private GuiSlider sRed;
+	private GuiSlider sGreen;
+	private GuiSlider sBlue;
+	private GuiButton bSetBladeColor;
+	private GuiButton bSetCoreColor;
 
 	public GuiLightsaberForge(EntityPlayer player, InventoryPlayer inventoryPlayer, TileLightsaberForge tile)
 	{
@@ -42,6 +53,11 @@ public class GuiLightsaberForge extends GuiContainer
 		buttonList.clear();
 
 		buttonList.add(cbSaberUnstable = new GuiCheckBox(0, guiLeft + 47, guiTop + 15, "Unstable", false));
+		buttonList.add(sRed = new GuiSlider(1, guiLeft + 47, guiTop + 30, 128, 20, "R: ", "", 0, 255, 0, false, true));
+		buttonList.add(sGreen = new GuiSlider(2, guiLeft + 47, guiTop + 51, 128, 20, "G: ", "", 0, 255, 0, false, true));
+		buttonList.add(sBlue = new GuiSlider(3, guiLeft + 47, guiTop + 72, 128, 20, "B: ", "", 0, 255, 0, false, true));
+		buttonList.add(bSetBladeColor = new GuiButton(4, guiLeft + 47, guiTop + 93, 63, 20, "Set Blade"));
+		buttonList.add(bSetCoreColor = new GuiButton(5, guiLeft + 113, guiTop + 93, 63, 20, "Set Core"));
 
 		lightsaberData = null;
 		setButtonsEnabled(false);
@@ -56,8 +72,10 @@ public class GuiLightsaberForge extends GuiContainer
 		{
 			if (lightsaberData == null)
 				lightsaberData = new LightsaberData(tile.getLightsaber());
+			LightsaberDescriptor d = lightsaberData.descriptor;
 
 			cbSaberUnstable.setIsChecked(lightsaberData.descriptor.unstable);
+
 			setButtonsEnabled(true);
 		}
 		else
@@ -71,6 +89,9 @@ public class GuiLightsaberForge extends GuiContainer
 	private void setButtonsEnabled(boolean enabled)
 	{
 		cbSaberUnstable.enabled = enabled;
+		sRed.enabled = enabled;
+		sGreen.enabled = enabled;
+		sBlue.enabled = enabled;
 	}
 
 	@Override
@@ -81,6 +102,16 @@ public class GuiLightsaberForge extends GuiContainer
 		if (button.id == cbSaberUnstable.id)
 		{
 			lightsaberData.descriptor.unstable = cbSaberUnstable.isChecked();
+			writeDescriptor();
+		}
+		else if (button.id == bSetBladeColor.id)
+		{
+			lightsaberData.descriptor.bladeColor = Fx.Util.GetRgb(sRed.getValueInt(), sGreen.getValueInt(), sBlue.getValueInt());
+			writeDescriptor();
+		}
+		else if (button.id == bSetCoreColor.id)
+		{
+			lightsaberData.descriptor.coreColor = Fx.Util.GetRgb(sRed.getValueInt(), sGreen.getValueInt(), sBlue.getValueInt());
 			writeDescriptor();
 		}
 	}
@@ -101,6 +132,13 @@ public class GuiLightsaberForge extends GuiContainer
 		{
 			fontRendererObj.drawString("sabers!", 47, 5, 4210752);
 		}
+
+		GL.Disable(EnableCap.Texture2D);
+		GL.Color(GLPalette.BLACK);
+		Fx.D2.DrawSolidRectangle(179, 29, 64, 64);
+		GL.Color(sRed.getValueInt() / 255f, sGreen.getValueInt() / 255f, sBlue.getValueInt() / 255f);
+		Fx.D2.DrawSolidRectangle(180, 30, 62, 62);
+		GL.Enable(EnableCap.Texture2D);
 
 		//		ArrayList<String> lines = new ArrayList<>();
 		//		lines.add(String.valueOf(mouseX - guiLeft));
