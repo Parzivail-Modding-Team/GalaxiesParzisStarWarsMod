b6d280bc153dcd67535f1f5eb254375605640283
parzivail <colby.newman2000@gmail.com>
2018-12-30 16:43:21 -0500

    clarify debug wizard use ingame

--- src/main/java/com/parzivail/swg/item/ItemDebugWizard.java
+++ src/main/java/com/parzivail/swg/item/ItemDebugWizard.java
@@ -1,17 +1,29 @@
 package com.parzivail.swg.item;
 
+import com.parzivail.swg.Resources;
 import com.parzivail.swg.force.Cron;
 import com.parzivail.swg.player.PswgExtProp;
+import com.parzivail.swg.proxy.Client;
 import com.parzivail.swg.registry.ForceRegistry;
+import net.minecraft.client.resources.I18n;
 import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.item.ItemStack;
 import net.minecraft.world.World;
+
+import java.util.List;
 
 public class ItemDebugWizard extends PItem
 {
 	public ItemDebugWizard()
 	{
 		super("debugWizard");
+	}
+
+	@Override
+	public void addInformation(ItemStack stack, EntityPlayer player, List info, boolean advancedTooltips)
+	{
+		info.add("Currently debugging: Force Powers (jump)");
+		info.add(String.format("%s: §r[§e%s§r]", I18n.format(Resources.guiDot("use")), Resources.getKeyName(Client.mc.gameSettings.keyBindUseItem)));
 	}
 
 	@Override
@@ -28,7 +40,7 @@ public class ItemDebugWizard extends PItem
 			}
 		}
 
-		Cron.usePower(player, ForceRegistry.fpLightning);
+		Cron.usePower(player, ForceRegistry.fpJump);
 
 		return stack;
 	}
