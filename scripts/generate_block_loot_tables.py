f24d60fd319fa717ace7270838733b4525e8933e
kb1000 <kaeptmblaubaer1000@gmail.com>
2020-10-30 17:38:38 +0100

    Fix ore block drops

--- scripts/generate_block_loot_tables.py
+++ scripts/generate_block_loot_tables.py
@@ -1,8 +1,21 @@
 import pathlib
 
-directory = pathlib.Path(__file__).parent.parent / "src" / "main" / "resources" / "data" / "pswg" / "loot_tables" / "blocks"
+directory = pathlib.Path(
+    __file__).parent.parent / "src" / "main" / "resources" / "data" / "pswg" / "loot_tables" / "blocks"
 
-blocks = ["sand_tatooine"]
+blocks = [
+    "ore_chromium",
+    "ore_cortosis",
+    "ore_diatium",
+    "ore_exonium",
+    "ore_helicite",
+    "ore_ionite",
+    "ore_kelerium",
+    "ore_rubindum",
+    "ore_thorolide",
+    "ore_titanium",
+    "sand_tatooine",
+]
 
 for block in blocks:
     with open(directory / f"{block}.json", "w", newline="\n", encoding="utf-8") as fp:
