607678dbb5a595e6f820e0d4f6a36600da4506e8
parzivail <colby.newman2000@gmail.com>
2020-11-01 22:51:01 -0500

    add biths M/F, temporarily remove chagrian

---
+++ src/main/java/com/parzivail/pswg/client/model/npc/ModelBithCombined.java
@@ -0,0 +1,49 @@
+package com.parzivail.pswg.client.model.npc;
+
+import net.minecraft.client.model.ModelPart;
+import net.minecraft.client.render.entity.model.PlayerEntityModel;
+import net.minecraft.entity.LivingEntity;
+
+public class ModelBithCombined<T extends LivingEntity> extends PlayerEntityModel<T>
+{
+	private static final int TEXTURE_WIDTH = 96;
+	private static final int TEXTURE_HEIGHT = 96;
+
+	public ModelPart HeadExtension;
+	public ModelPart FrontalR;
+	public ModelPart FrontalL;
+	public ModelPart Chest;
+
+	public ModelBithCombined(boolean male, float scale)
+	{
+		super(scale, true);
+
+		textureWidth = TEXTURE_WIDTH;
+		textureHeight = TEXTURE_HEIGHT;
+
+		this.HeadExtension = new ModelPart(this, 0, 74);
+		this.HeadExtension.setPivot(-4.5F, -9.3F, -3.4F);
+		this.HeadExtension.addCuboid(0.0F, 0.0F, 0.0F, 9, 7, 8, 0.0F);
+		this.Chest = new ModelPart(this, 0, 65);
+		this.Chest.setPivot(0.0F, -0.1F, 1.0F);
+		this.Chest.addCuboid(-3.0F, 2.0F, -4.0F, 6, 3, 2, 0.0F);
+		this.FrontalL = new ModelPart(this, 47, 80);
+		this.FrontalL.setPivot(4.7F, 0.2F, -1.2F);
+		this.FrontalL.addCuboid(0.0F, 0.0F, 0.0F, 4, 5, 4, 0.0F);
+		this.FrontalR = new ModelPart(this, 47, 80);
+		this.FrontalR.setPivot(4.3F, 0.2F, -1.2F);
+		this.FrontalR.addCuboid(-4.0F, 0.0F, 0.0F, 4, 5, 4, 0.0F);
+		this.head.addChild(this.HeadExtension);
+		if (!male)
+			this.torso.addChild(this.Chest);
+		this.HeadExtension.addChild(this.FrontalL);
+		this.HeadExtension.addChild(this.FrontalR);
+	}
+
+	@Override
+	public void accept(ModelPart modelPart)
+	{
+		modelPart.setTextureSize(TEXTURE_WIDTH, TEXTURE_HEIGHT);
+		super.accept(modelPart);
+	}
+}
--- src/main/java/com/parzivail/pswg/client/species/SwgSpeciesModels.java
+++ src/main/java/com/parzivail/pswg/client/species/SwgSpeciesModels.java
@@ -1,7 +1,10 @@
 package com.parzivail.pswg.client.species;
 
 import com.parzivail.pswg.Resources;
-import com.parzivail.pswg.client.model.npc.*;
+import com.parzivail.pswg.client.model.npc.ModelBithCombined;
+import com.parzivail.pswg.client.model.npc.ModelTogrutaF;
+import com.parzivail.pswg.client.model.npc.ModelTogrutaM;
+import com.parzivail.pswg.client.model.npc.ModelTwilekCombined;
 import com.parzivail.pswg.container.SwgSpecies;
 import net.minecraft.util.Identifier;
 
@@ -15,10 +18,12 @@ public class SwgSpeciesModels
 	{
 		register(new SwgSpeciesModel(SwgSpecies.SPECIES_TOGRUTA_M, Resources.identifier("textures/species/togruta/male/yellow.png"), new ModelTogrutaM<>(0)));
 		register(new SwgSpeciesModel(SwgSpecies.SPECIES_TOGRUTA_F, Resources.identifier("textures/species/togruta/female/yellow.png"), new ModelTogrutaF<>(0)));
-		register(new SwgSpeciesModel(SwgSpecies.SPECIES_CHAGRIAN_M, Resources.identifier("textures/species/chagrian_m.png"), new ModelChagrianM<>(0)));
-		register(new SwgSpeciesModel(SwgSpecies.SPECIES_CHAGRIAN_F, Resources.identifier("textures/species/chagrian_f.png"), new ModelChagrianF<>(0)));
+		//		register(new SwgSpeciesModel(SwgSpecies.SPECIES_CHAGRIAN_M, Resources.identifier("textures/species/chagrian_m.png"), new ModelChagrianM<>(0)));
+		//		register(new SwgSpeciesModel(SwgSpecies.SPECIES_CHAGRIAN_F, Resources.identifier("textures/species/chagrian_f.png"), new ModelChagrianF<>(0)));
 		register(new SwgSpeciesModel(SwgSpecies.SPECIES_TWILEK_M, Resources.identifier("textures/species/twilek/male/green.png"), new ModelTwilekCombined<>(true, 0)));
 		register(new SwgSpeciesModel(SwgSpecies.SPECIES_TWILEK_F, Resources.identifier("textures/species/twilek/female/green.png"), new ModelTwilekCombined<>(false, 0)));
+		register(new SwgSpeciesModel(SwgSpecies.SPECIES_BITH_M, Resources.identifier("textures/species/bith/male/pink.png"), new ModelBithCombined<>(true, 0)));
+		register(new SwgSpeciesModel(SwgSpecies.SPECIES_BITH_F, Resources.identifier("textures/species/bith/female/pink.png"), new ModelBithCombined<>(false, 0)));
 	}
 
 	private static void register(SwgSpeciesModel model)
