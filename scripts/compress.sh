86f40975114947a4971cf22cf203b02e7be0adaf
kb1000 <kaeptmblaubaer1000@gmail.com>
2020-11-23 18:27:16 +0100

    Compress textures using zopflipng

---
+++ scripts/compress.sh
@@ -0,0 +1,7 @@
+export LC_ALL=C
+before="$(find src/main/resources -regex ".*\\.png" -print0 | du -bch --files0-from=- | grep total)"
+find src/main/resources -regex ".*\\.png" -print0 | xargs -P 0 -0 -I {} zopflipng --lossy_8bit --lossy_transparent --filters=01234mepb -y --iterations=10 {} {}
+echo Before:
+echo $before
+echo After:
+find src/main/resources -regex ".*\\.png" -print0 | du -bch --files0-from=- | grep total
