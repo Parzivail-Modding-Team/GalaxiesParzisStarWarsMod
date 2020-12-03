fa1854689b8625f1eb7bd5be971d996930531d9a
parzivail <colby.newman2000@gmail.com>
2020-12-03 12:56:53 -0500

    fix thrown lightsaber gimbal lock

--- src/main/java/com/parzivail/pswg/item/DebugItem.java
+++ src/main/java/com/parzivail/pswg/item/DebugItem.java
@@ -7,6 +7,7 @@ import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
 import net.minecraft.util.Hand;
 import net.minecraft.util.TypedActionResult;
+import net.minecraft.util.math.MathHelper;
 import net.minecraft.world.World;
 
 public class DebugItem extends Item
@@ -20,7 +21,7 @@ public class DebugItem extends Item
 	public TypedActionResult<ItemStack> use(World world, PlayerEntity player, Hand hand)
 	{
 		final ThrownLightsaberEntity entity = new ThrownLightsaberEntity(SwgEntities.Misc.ThrownLightsaber, player, world);
-		entity.setProperties(player, player.pitch, player.yaw, 0.0F, 0.6f, 0);
+		entity.setProperties(player, MathHelper.clamp(player.pitch, -89.9f, 89.9f), player.yaw, 0.0F, 0.6f, 0);
 		world.spawnEntity(entity);
 
 		return TypedActionResult.consume(player.getStackInHand(hand));
