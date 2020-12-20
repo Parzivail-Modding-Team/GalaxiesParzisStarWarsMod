7764d56bb19be0f979a514f3df6fc84dab71cec5
kb1000 <kaeptmblaubaer1000@gmail.com>
2020-12-20 02:05:36 +0100

    Add en_us strings for config GUI

--- src/main/java/com/parzivail/pswg/Config.java
+++ src/main/java/com/parzivail/pswg/Config.java
@@ -7,14 +7,17 @@ import me.sargunvohra.mcmods.autoconfig1u.shadowed.blue.endless.jankson.Comment;
 @me.sargunvohra.mcmods.autoconfig1u.annotation.Config(name = "pswg")
 public class Config implements ConfigData
 {
+	// @Comments are replicated in en_us.json (for i18n)
 	public static class Input
 	{
 		@Comment("Sets whether mouse inputs should be pitch and yaw, or pitch and roll")
+		@ConfigEntry.Gui.Tooltip
 		public boolean shipRollPriority = false;
 	}
 
 	@ConfigEntry.Gui.CollapsibleObject
 	@Comment("Here you can change your preferred input methods.")
+	@ConfigEntry.Gui.Tooltip
 	public Input input = new Input();
 
 	public void reload()
