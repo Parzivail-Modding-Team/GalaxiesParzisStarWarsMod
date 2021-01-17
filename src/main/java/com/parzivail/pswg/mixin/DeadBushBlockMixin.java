18f712cf67bbdde4a81e0c7f40f1d2ec33285d9b
kb1000 <kaeptmblaubaer1000@gmail.com>
2021-01-17 01:07:32 +0100

    Make dead bushes placeable on tatooine sand

---
+++ src/main/java/com/parzivail/pswg/mixin/DeadBushBlockMixin.java
@@ -0,0 +1,22 @@
+package com.parzivail.pswg.mixin;
+
+import com.parzivail.pswg.container.SwgBlocks;
+import net.minecraft.block.BlockState;
+import net.minecraft.block.DeadBushBlock;
+import net.minecraft.util.math.BlockPos;
+import net.minecraft.world.BlockView;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
+
+@Mixin(DeadBushBlock.class)
+public class DeadBushBlockMixin
+{
+	@Inject(method = "canPlantOnTop", at = @At("RETURN"), cancellable = true)
+	private void canPlantOnTatooineSand(BlockState floor, BlockView world, BlockPos pos, CallbackInfoReturnable<Boolean> cir) {
+		if (floor.getBlock() == SwgBlocks.Sand.Tatooine) {
+			cir.setReturnValue(true);
+		}
+	}
+}
