b4e39897a62592300f4c462619098bfff661dd56
parzivail <colby.newman2000@gmail.com>
2021-01-24 19:37:08 -0500

    add tatooine canyon sand

--- src/main/java/com/parzivail/pswg/container/SwgBlocks.java
+++ src/main/java/com/parzivail/pswg/container/SwgBlocks.java
@@ -236,6 +236,8 @@ public class SwgBlocks
 		public static final Block Tatooine = new FallingBlock(FabricBlockSettings.of(Material.AGGREGATE).strength(0.5F).sounds(BlockSoundGroup.SAND));
 		@RegistryName("dense_tatooine_sand")
 		public static final Block DenseTatooine = new FallingBlock(FabricBlockSettings.of(Material.AGGREGATE).strength(0.5F).sounds(BlockSoundGroup.SAND));
+		@RegistryName("tatooine_canyon_sand")
+		public static final Block TatooineCanyon = new FallingBlock(FabricBlockSettings.of(Material.AGGREGATE).strength(0.5F).sounds(BlockSoundGroup.SAND));
 	}
 
 	public static class Stone
