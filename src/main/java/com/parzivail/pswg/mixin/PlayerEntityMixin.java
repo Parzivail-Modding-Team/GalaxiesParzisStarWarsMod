77fbf9aac634a0b2d5595350ab3ec52cf307c053
kb1000 <kaeptmblaubaer1000@gmail.com>
2021-01-26 18:48:45 +0100

    Disable blaster scope when switching items

--- src/main/java/com/parzivail/pswg/mixin/PlayerEntityMixin.java
+++ src/main/java/com/parzivail/pswg/mixin/PlayerEntityMixin.java
@@ -2,14 +2,18 @@ package com.parzivail.pswg.mixin;
 
 import com.parzivail.pswg.Client;
 import com.parzivail.pswg.client.sound.LightsaberIdleSoundInstance;
+import com.parzivail.pswg.container.SwgItems;
+import com.parzivail.pswg.item.blaster.data.BlasterTag;
 import net.fabricmc.api.EnvType;
 import net.fabricmc.api.Environment;
 import net.minecraft.entity.player.PlayerEntity;
+import net.minecraft.item.ItemStack;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Unique;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+import org.spongepowered.asm.mixin.injection.callback.LocalCapture;
 
 @Mixin(PlayerEntity.class)
 @Environment(EnvType.CLIENT)
@@ -31,5 +35,15 @@ public class PlayerEntityMixin
 			Client.minecraft.getSoundManager().play(new LightsaberIdleSoundInstance(player));
 		}
 		metConditionsForLightsaberSound = meetsConditionsForLightsaberSound;
+	}
+
+	@Inject(method = "tick", at = @At(value = "INVOKE", target = "Lnet/minecraft/entity/player/PlayerEntity;resetLastAttackedTicks()V"), locals = LocalCapture.CAPTURE_FAILHARD)
+	private void onSwitchItem(CallbackInfo ci, int i, double d, ItemStack itemStack)
+	{
+		if (itemStack.getItem() == SwgItems.Blaster.Blaster) {
+			if (itemStack.hasTag()) {
+				BlasterTag.mutate(itemStack, tag -> tag.isAimingDownSights = false);
+			}
+		}
 	}
 }
