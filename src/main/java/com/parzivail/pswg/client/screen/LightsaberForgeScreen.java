ec66cc801f6801f1b0d91c9fe2fbd3f8d45f3d2f
parzivail <colby.newman2000@gmail.com>
2021-05-27 23:03:47 -0400

    fix lightsaber forge screen dimming

--- src/main/java/com/parzivail/pswg/client/screen/LightsaberForgeScreen.java
+++ src/main/java/com/parzivail/pswg/client/screen/LightsaberForgeScreen.java
@@ -267,8 +267,6 @@ public class LightsaberForgeScreen extends HandledScreen<LightsaberForgeScreenHa
 
 	protected void drawBackground(MatrixStack matrices, float delta, int mouseX, int mouseY)
 	{
-		this.renderBackground(matrices);
-
 		RenderSystem.setShader(GameRenderer::getPositionTexShader);
 		RenderSystem.setShaderColor(1.0F, 1.0F, 1.0F, 1.0F);
 		RenderSystem.setShaderTexture(0, TEXTURE);
