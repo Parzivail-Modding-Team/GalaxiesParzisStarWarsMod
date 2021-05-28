aafa440e5bab88bc7ac7d4e9cec26ae2dbad9366
parzivail <colby.newman2000@gmail.com>
2021-05-28 19:02:41 -0400

    reimplement worrt, faa, and laa rendering

--- src/main/java/com/parzivail/pswg/client/render/fish/LaaEntityRenderer.java
+++ src/main/java/com/parzivail/pswg/client/render/fish/LaaEntityRenderer.java
@@ -1,28 +1,38 @@
 package com.parzivail.pswg.client.render.fish;
 
-//public class LaaEntityRenderer extends MobEntityRenderer<FishEntity, LaaModel<FishEntity>>
-//{
-//	public LaaEntityRenderer(EntityRenderDispatcher entityRenderDispatcher)
-//	{
-//		super(entityRenderDispatcher, new LaaModel<>(), 0.5f);
-//	}
-//
-//	@Override
-//	public Identifier getTexture(FishEntity entity)
-//	{
-//		return Resources.identifier("textures/entity/fish/laa.png");
-//	}
-//
-//	@Override
-//	protected void setupTransforms(FishEntity entity, MatrixStack matrixStack, float f, float g, float h)
-//	{
-//		super.setupTransforms(entity, matrixStack, f, g, h);
-//		float i = 4.3F * MathHelper.sin(0.6F * f);
-//		matrixStack.multiply(Vec3f.POSITIVE_Y.getDegreesQuaternion(i));
-//		if (!entity.isTouchingWater())
-//		{
-//			matrixStack.translate(0.10000000149011612D, 0.10000000149011612D, -0.10000000149011612D);
-//			matrixStack.multiply(Vec3f.POSITIVE_Z.getDegreesQuaternion(90.0F));
-//		}
-//	}
-//}
+import com.parzivail.pswg.Resources;
+import com.parzivail.pswg.client.model.fish.LaaEntityModel;
+import net.minecraft.client.render.entity.EntityRendererFactory;
+import net.minecraft.client.render.entity.MobEntityRenderer;
+import net.minecraft.client.util.math.MatrixStack;
+import net.minecraft.entity.passive.FishEntity;
+import net.minecraft.util.Identifier;
+import net.minecraft.util.math.MathHelper;
+import net.minecraft.util.math.Vec3f;
+
+public class LaaEntityRenderer extends MobEntityRenderer<FishEntity, LaaEntityModel<FishEntity>>
+{
+	public LaaEntityRenderer(EntityRendererFactory.Context context)
+	{
+		super(context, new LaaEntityModel<>(), 0.5f);
+	}
+
+	@Override
+	public Identifier getTexture(FishEntity entity)
+	{
+		return Resources.identifier("textures/entity/fish/laa.png");
+	}
+
+	@Override
+	protected void setupTransforms(FishEntity entity, MatrixStack matrixStack, float f, float g, float h)
+	{
+		super.setupTransforms(entity, matrixStack, f, g, h);
+		float i = 4.3F * MathHelper.sin(0.6F * f);
+		matrixStack.multiply(Vec3f.POSITIVE_Y.getDegreesQuaternion(i));
+		if (!entity.isTouchingWater())
+		{
+			matrixStack.translate(0.10000000149011612D, 0.10000000149011612D, -0.10000000149011612D);
+			matrixStack.multiply(Vec3f.POSITIVE_Z.getDegreesQuaternion(90.0F));
+		}
+	}
+}
