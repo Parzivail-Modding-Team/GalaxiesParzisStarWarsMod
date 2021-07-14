9c52a73c74d77e05c1f63916bc6153c069ce94a3
parzivail <colby.newman2000@gmail.com>
2021-07-13 21:56:50 -0400

    remove stencil mixins, add back species rendering mixins

--- src/main/java/com/parzivail/pswg/mixin/AbstractClientPlayerEntityMixin.java
+++ src/main/java/com/parzivail/pswg/mixin/AbstractClientPlayerEntityMixin.java
@@ -1,24 +1,31 @@
 package com.parzivail.pswg.mixin;
 
+import com.parzivail.pswg.client.species.SwgSpeciesModels;
+import com.parzivail.pswg.component.SwgEntityComponents;
 import net.fabricmc.api.EnvType;
 import net.fabricmc.api.Environment;
 import net.minecraft.client.network.AbstractClientPlayerEntity;
+import net.minecraft.entity.player.PlayerEntity;
+import net.minecraft.util.Identifier;
 import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
 @Mixin(AbstractClientPlayerEntity.class)
 @Environment(EnvType.CLIENT)
 public class AbstractClientPlayerEntityMixin
 {
-//	@Inject(method = "getSkinTexture", at = @At(value = "HEAD"), cancellable = true)
-//	private void getSkinTexture(CallbackInfoReturnable<Identifier> cir)
-//	{
-//		SwgPersistentComponents pc = SwgEntityComponents.getPersistent((PlayerEntity)(Object)this);
-//
-//		SwgSpecies species = pc.getSpecies();
-//		if (species == null)
-//			return;
-//
-//		cir.setReturnValue(SwgSpeciesModels.getTexture(species));
-//		cir.cancel();
-//	}
+	@Inject(method = "getSkinTexture", at = @At(value = "HEAD"), cancellable = true)
+	private void getSkinTexture(CallbackInfoReturnable<Identifier> cir)
+	{
+		var pc = SwgEntityComponents.getPersistent((PlayerEntity)(Object)this);
+
+		var species = pc.getSpecies();
+		if (species == null)
+			return;
+
+		cir.setReturnValue(SwgSpeciesModels.getTexture(species));
+		cir.cancel();
+	}
 }
