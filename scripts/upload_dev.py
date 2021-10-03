d907c359c9fe6d6bdfaead00270fe902dd11ccc0
parzivail <colby.newman2000@gmail.com>
2021-10-03 16:44:15 -0400

    fix python syntax

--- scripts/upload_dev.py
+++ scripts/upload_dev.py
@@ -6,11 +6,11 @@ import requests
 
 gittags = subprocess.check_output("git describe --tags").strip()
 file = f"pswg-{gittags}.jar"
-if os.path.isfile(file)
+if os.path.isfile(file):
     with open(file, "rb") as fp:
         with requests.post(sys.argv[1], files={"file": fp}, data={"content": f"Build {os.environ['BUILD_NUMBER']} built! <:mc_cake:711406798292254792>"}) as resp:
             print("discord response: ", resp.text)
         subprocess.check_call(["curl", "--location", "--resolve=kb1000.de:443:10.244.44.214", "--request", "POST", sys.argv[2], "--form", f"file=@{file}"])
-else
+else:
     with requests.post(sys.argv[1], data={"content": f"Build {os.environ['BUILD_NUMBER']} failed! :x:"}) as resp:
         print("discord response: ", resp.text)
