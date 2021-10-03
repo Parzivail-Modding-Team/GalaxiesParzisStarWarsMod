0cf9964b25eacde3f2123260b5a7c1a4cd20fa98
kb1000 <kaeptmblaubaer1000@gmail.com>
2021-10-03 23:47:28 +0200

    Fix curl command line for the server updater

--- scripts/upload_dev.py
+++ scripts/upload_dev.py
@@ -14,7 +14,7 @@ if os.path.isfile(file):
     with open(file, "rb") as fp:
         with requests.post(sys.argv[1], files={"file": fp}, data={"content": f"Build {os.environ['BUILD_NUMBER']} built! <:mc_cake:711406798292254792>"}) as resp:
             print("discord response:", resp.text)
-        subprocess.check_call(["curl", "--location", "--resolve=kb1000.de:443:10.244.44.214", "--request", "POST", sys.argv[2], "--form", f"file=@{file}"])
+        subprocess.check_call(["curl", "--location", "--resolve", "kb1000.de:443:10.244.44.214", "--request", "POST", sys.argv[2], "--form", f"file=@{file}"])
 else:
     print("Did not find file")
     with requests.post(sys.argv[1], data={"content": f"Build {os.environ['BUILD_NUMBER']} failed! \N{CROSS MARK}"}) as resp:
