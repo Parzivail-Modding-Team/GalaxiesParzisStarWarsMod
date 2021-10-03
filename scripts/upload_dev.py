e9a487038a1b8882623a6a3d0f3253a57b0f3bf2
kb1000 <kaeptmblaubaer1000@gmail.com>
2021-10-03 23:41:06 +0200

    Add more logging to the upload script

--- scripts/upload_dev.py
+++ scripts/upload_dev.py
@@ -4,18 +4,18 @@ import sys
 
 import requests
 
-# Ensure working directory is correct
-os.chdir(sys.path[0])
-
 print("Current directory: " + os.getcwd())
 
 gittags = subprocess.check_output("git describe --tags").strip()
 file = f"pswg-{gittags}.jar"
+print("Trying to upload", os.path.realpath(file))
 if os.path.isfile(file):
+    print("Found file")
     with open(file, "rb") as fp:
         with requests.post(sys.argv[1], files={"file": fp}, data={"content": f"Build {os.environ['BUILD_NUMBER']} built! <:mc_cake:711406798292254792>"}) as resp:
-            print("discord response: ", resp.text)
+            print("discord response:", resp.text)
         subprocess.check_call(["curl", "--location", "--resolve=kb1000.de:443:10.244.44.214", "--request", "POST", sys.argv[2], "--form", f"file=@{file}"])
 else:
+    print("Did not find file")
     with requests.post(sys.argv[1], data={"content": f"Build {os.environ['BUILD_NUMBER']} failed! \N{CROSS MARK}"}) as resp:
-        print("discord response: ", resp.text)
+        print("discord response:", resp.text)
