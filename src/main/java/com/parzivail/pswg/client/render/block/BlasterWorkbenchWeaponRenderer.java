6b38aaff45fd709dd37412edff742fc670de41a6
parzivail <colby.newman2000@gmail.com>
2022-02-20 12:33:22 -0500

    lay blaster down on workbench (WIP positioning)

--- src/main/java/com/parzivail/pswg/client/render/block/BlasterWorkbenchWeaponRenderer.java
+++ src/main/java/com/parzivail/pswg/client/render/block/BlasterWorkbenchWeaponRenderer.java
@@ -3,6 +3,8 @@ package com.parzivail.pswg.client.render.block;
 import com.parzivail.pswg.blockentity.BlasterWorkbenchBlockEntity;
 import com.parzivail.pswg.client.render.item.BlasterItemRenderer;
 import com.parzivail.pswg.container.SwgBlocks;
+import com.parzivail.pswg.item.blaster.BlasterItem;
+import com.parzivail.pswg.item.blaster.data.BlasterArchetype;
 import com.parzivail.util.block.rotating.RotatingBlock;
 import com.parzivail.util.math.ClientMathUtil;
 import net.minecraft.client.render.VertexConsumerProvider;
@@ -10,6 +12,8 @@ import net.minecraft.client.render.block.entity.BlockEntityRenderer;
 import net.minecraft.client.render.block.entity.BlockEntityRendererFactory;
 import net.minecraft.client.render.model.json.ModelTransformation;
 import net.minecraft.client.util.math.MatrixStack;
+import net.minecraft.util.math.Quaternion;
+import net.minecraft.util.math.Vec3f;
 
 public class BlasterWorkbenchWeaponRenderer implements BlockEntityRenderer<BlasterWorkbenchBlockEntity>
 {
@@ -39,7 +43,17 @@ public class BlasterWorkbenchWeaponRenderer implements BlockEntityRenderer<Blast
 
 		matrices.translate(0.5, 0, 0.5);
 		matrices.multiply(ClientMathUtil.getRotation(rotation));
-		matrices.translate(2 / 16.0, 15 / 16.0, -7 / 16.0);
+
+		var desc = BlasterItem.getBlasterDescriptor(blaster);
+
+		// TODO: move further based on blaster length?
+		if (desc.type == BlasterArchetype.PISTOL)
+			matrices.translate(3 / 16.0, 14 / 16.0, -5 / 16.0);
+		else
+			matrices.translate(5 / 16.0, 14 / 16.0, -3 / 16.0);
+
+		matrices.multiply(new Quaternion(Vec3f.POSITIVE_Y, 10, true));
+		matrices.multiply(new Quaternion(Vec3f.POSITIVE_Z, 80, true));
 
 		BlasterItemRenderer.INSTANCE.render(blaster, ModelTransformation.Mode.NONE, false, matrices, vertexConsumers, light, overlay, null);
 
