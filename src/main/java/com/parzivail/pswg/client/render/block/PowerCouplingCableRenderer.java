a0bdf1ce58b963056476c7084f7676fc4116a10d
parzivail <colby.newman2000@gmail.com>
2022-03-28 17:53:43 -0400

    make cable rendering tension-based in prep for cable tensioner item

--- src/main/java/com/parzivail/pswg/client/render/block/PowerCouplingCableRenderer.java
+++ src/main/java/com/parzivail/pswg/client/render/block/PowerCouplingCableRenderer.java
@@ -70,49 +70,69 @@ public class PowerCouplingCableRenderer implements BlockEntityRenderer<PowerCoup
 	private void renderCable(World world, Vec3d source, Vec3d dest, float tickDelta, MatrixStack matrices, VertexConsumerProvider provider)
 	{
 		matrices.push();
-		var j = (float)(dest.x - source.x);
-		var k = (float)(dest.y - source.y);
-		var l = (float)(dest.z - source.z);
-		var m = 0.05F;
+		var dX = (float)(dest.x - source.x);
+		var dY = (float)(dest.y - source.y);
+		var dZ = (float)(dest.z - source.z);
+		var size = 0.04f;
 		var vertexConsumer = provider.getBuffer(RenderLayer.getLeash());
 		var matrix4f = matrices.peek().getPositionMatrix();
-		var n = MathHelper.fastInverseSqrt(j * j + l * l) * m / 2.0F;
-		var o = l * n;
-		var p = j * n;
-		var blockPos = new BlockPos(source);
-		var blockPos2 = new BlockPos(dest);
-		int q = world.getLightLevel(LightType.BLOCK, blockPos);
-		int r = world.getLightLevel(LightType.BLOCK, blockPos2);
-		int s = world.getLightLevel(LightType.SKY, blockPos);
-		int t = world.getLightLevel(LightType.SKY, blockPos2);
+		var n = MathHelper.fastInverseSqrt(dX * dX + dZ * dZ) * size / 2.0F;
+		var o = dZ * n;
+		var p = dX * n;
+		var srcPos = new BlockPos(source);
+		var dstPos = new BlockPos(dest);
+		int srcBlockLight = world.getLightLevel(LightType.BLOCK, srcPos);
+		int dstBlockLight = world.getLightLevel(LightType.BLOCK, dstPos);
+		int srcSkyLight = world.getLightLevel(LightType.SKY, srcPos);
+		int dstSkyLight = world.getLightLevel(LightType.SKY, dstPos);
 
-		for (var u = 0; u <= 12; ++u)
-		{
-			renderCableSegment(vertexConsumer, matrix4f, j, k, l, q, r, s, t, m, m, o, p, u, false);
-		}
+		// insert degenerate triangle into strip
+		vertexConsumer.vertex(matrix4f, 0, 0, 0).color(0).light(0).next();
+		vertexConsumer.vertex(matrix4f, 0, 0, 0).color(0).light(0).next();
 
-		for (var u = 12; u >= 0; --u)
-		{
-			renderCableSegment(vertexConsumer, matrix4f, j, k, l, q, r, s, t, m, 0.0F, o, p, u, true);
-		}
+		for (var u = 0; u <= 24; ++u)
+			renderCableSegment(world, tickDelta, vertexConsumer, matrix4f, dX, dY, dZ, srcBlockLight, dstBlockLight, srcSkyLight, dstSkyLight, size, size, o, p, u, false);
+
+		for (var u = 24; u >= 0; --u)
+			renderCableSegment(world, tickDelta, vertexConsumer, matrix4f, dX, dY, dZ, srcBlockLight, dstBlockLight, srcSkyLight, dstSkyLight, size, 0.0F, o, p, u, true);
+
+		// insert degenerate triangle into strip
+		vertexConsumer.vertex(matrix4f, 0, 0, 0).color(0).light(0).next();
+		vertexConsumer.vertex(matrix4f, 0, 0, 0).color(0).light(0).next();
 
 		matrices.pop();
 	}
 
-	private static void renderCableSegment(VertexConsumer vertexConsumer, Matrix4f positionMatrix, float f, float g, float h, int leashedEntityBlockLight, int holdingEntityBlockLight, int leashedEntitySkyLight, int holdingEntitySkyLight, float i, float j, float k, float l, int pieceIndex, boolean isLeashKnot)
+	private static void renderCableSegment(World world, float tickDelta, VertexConsumer vertexConsumer, Matrix4f positionMatrix, float dX, float dY, float dZ, int leashedEntityBlockLight, int holdingEntityBlockLight, int leashedEntitySkyLight, int holdingEntitySkyLight, float i, float j, float k, float l, int pieceIndex, boolean isLeashKnot)
 	{
-		var m = (float)pieceIndex / 12.0F;
-		var n = (int)MathHelper.lerp(m, (float)leashedEntityBlockLight, (float)holdingEntityBlockLight);
-		var o = (int)MathHelper.lerp(m, (float)leashedEntitySkyLight, (float)holdingEntitySkyLight);
-		var p = LightmapTextureManager.pack(n, o);
-		var u = f * m;
+		var t = (float)pieceIndex / 24.0F;
+		var blockLight = (int)MathHelper.lerp(t, (float)leashedEntityBlockLight, (float)holdingEntityBlockLight);
+		var skyLight = (int)MathHelper.lerp(t, (float)leashedEntitySkyLight, (float)holdingEntitySkyLight);
+		var packedLight = LightmapTextureManager.pack(blockLight, skyLight);
 
-		var distance = MathHelper.sqrt(f * f + g * g + h * h);
-		var lateralDistance = MathHelper.sqrt(f * f + h * h);
-		var sag = 0.6f;
-		var v = MathHelper.lerp((float)Math.pow(m, sag * distance / lateralDistance), -distance * m, g);
-		var w = h * m;
-		vertexConsumer.vertex(positionMatrix, u - k, v + j, w + l).color(0xFF202020).light(p).next();
-		vertexConsumer.vertex(positionMatrix, u + k, v + i - j, w - l).color(0xFF202020).light(p).next();
+		var p0 = new Vec3d(0, 0, 0);
+		var p1 = new Vec3d(dX, dY, dZ);
+
+		var T = 2;
+
+		var pC = p0.add(p1).multiply(0.5).subtract(0, T, 0);
+
+		var time = world.getTime();
+		time += (int)Math.abs(dX) ^ (31L * (int)Math.abs(dY)) ^ (63L * (int)Math.abs(dZ));
+		time %= 100;
+		var timeRads = (time + tickDelta) / 50f * MathHelper.PI;
+
+		var left = new Vec3d(dX, dY, dZ).normalize().crossProduct(new Vec3d(0, 1, 0)).multiply(0.1);
+		pC = pC.add(left.multiply(MathHelper.sin(timeRads)));
+
+		var oneMinusT = (1 - t);
+		var B = p0.multiply(oneMinusT * oneMinusT).add(pC.multiply(2 * t * oneMinusT)).add(p1.multiply(t * t));
+
+		var x = (float)B.x;
+		var y = (float)B.y;
+		var z = (float)B.z;
+
+		vertexConsumer.vertex(positionMatrix, x - k, y + j, z + l).color(0xFF202020).light(packedLight).next();
+		vertexConsumer.vertex(positionMatrix, x + k, y + i - j, z - l).color(0xFF202020).light(packedLight).next();
 	}
 }
