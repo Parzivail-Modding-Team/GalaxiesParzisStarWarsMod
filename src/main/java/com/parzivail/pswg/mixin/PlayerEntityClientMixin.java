aa28a8a75b02f4d19e3b0af23ac1122a368e9936
kb1000 <kaeptmblaubaer1000@gmail.com>
2022-05-01 21:48:10 +0200

    Fix lightsaber auto-extinguishing on dedicated servers

---
+++ src/main/java/com/parzivail/pswg/mixin/PlayerEntityClientMixin.java
@@ -0,0 +1,36 @@
+package com.parzivail.pswg.mixin;
+
+import com.parzivail.pswg.client.sound.LightsaberIdleSoundInstance;
+import net.fabricmc.api.EnvType;
+import net.fabricmc.api.Environment;
+import net.minecraft.client.MinecraftClient;
+import net.minecraft.entity.player.PlayerEntity;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Unique;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+
+@Mixin(PlayerEntity.class)
+@Environment(EnvType.CLIENT)
+public class PlayerEntityClientMixin
+{
+	@Unique
+	private boolean metConditionsForLightsaberSound = false;
+
+	@Inject(method = "tick", at = @At("HEAD"))
+	private void tick(CallbackInfo ci)
+	{
+		var player = (PlayerEntity)(Object)this;
+		if (!player.world.isClient)
+			return;
+
+		var meetsConditionsForLightsaberSound = LightsaberIdleSoundInstance.areConditionsMet(player);
+		if (meetsConditionsForLightsaberSound && !metConditionsForLightsaberSound)
+		{
+			var minecraft = MinecraftClient.getInstance();
+			minecraft.getSoundManager().play(new LightsaberIdleSoundInstance(player));
+		}
+		metConditionsForLightsaberSound = meetsConditionsForLightsaberSound;
+	}
+}
--- src/main/java/com/parzivail/pswg/mixin/PlayerEntityMixin.java
+++ src/main/java/com/parzivail/pswg/mixin/PlayerEntityMixin.java
@@ -1,14 +1,10 @@
 package com.parzivail.pswg.mixin;
 
-import com.parzivail.pswg.client.sound.LightsaberIdleSoundInstance;
 import com.parzivail.pswg.item.blaster.BlasterItem;
 import com.parzivail.pswg.item.blaster.data.BlasterTag;
 import com.parzivail.pswg.item.lightsaber.LightsaberItem;
 import com.parzivail.pswg.item.lightsaber.data.LightsaberTag;
 import com.parzivail.util.world.InventoryUtil;
-import net.fabricmc.api.EnvType;
-import net.fabricmc.api.Environment;
-import net.minecraft.client.MinecraftClient;
 import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.item.ItemStack;
 import org.spongepowered.asm.mixin.Mixin;
@@ -18,29 +14,10 @@ import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 @Mixin(PlayerEntity.class)
-@Environment(EnvType.CLIENT)
 public class PlayerEntityMixin
 {
 	@Unique
 	private ItemStack lastSelectedItemRef;
-	@Unique
-	private boolean metConditionsForLightsaberSound = false;
-
-	@Inject(method = "tick", at = @At("HEAD"))
-	private void tick(CallbackInfo ci)
-	{
-		var player = (PlayerEntity)(Object)this;
-		if (!player.world.isClient)
-			return;
-
-		var meetsConditionsForLightsaberSound = LightsaberIdleSoundInstance.areConditionsMet(player);
-		if (meetsConditionsForLightsaberSound && !metConditionsForLightsaberSound)
-		{
-			var minecraft = MinecraftClient.getInstance();
-			minecraft.getSoundManager().play(new LightsaberIdleSoundInstance(player));
-		}
-		metConditionsForLightsaberSound = meetsConditionsForLightsaberSound;
-	}
 
 	@Inject(method = "tick", at = @At(value = "INVOKE", target = "Lnet/minecraft/entity/player/PlayerEntity;getMainHandStack()Lnet/minecraft/item/ItemStack;", shift = At.Shift.BEFORE))
 	private void onTick(CallbackInfo ci)
