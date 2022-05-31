26c655b2294d59464ee6409d7e78d6328c5a4fd5
kb1000 <kaeptmblaubaer1000@gmail.com>
2022-06-01 00:26:36 +0200

    Update dependencies

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,8 +2,8 @@ import java.io.ByteArrayOutputStream
 import java.util.regex.Pattern
 
 plugins {
-	id("fabric-loom") version "0.11-SNAPSHOT"
-	id("io.github.juuxel.loom-quiltflower") version "1.7.1"
+	id("fabric-loom") version "0.12-SNAPSHOT"
+	id("io.github.juuxel.loom-quiltflower") version "1.7.2"
 	`maven-publish`
 }
 
