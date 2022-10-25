201cd74dc04f18debe43fcf07e5664bc80e89a8d
parzivail <colby.newman2000@gmail.com>
2022-10-24 23:48:42 -0400

    toolkit: integrate worldmesher in prep for worldgen tools

--- projects/toolkit/project.gradle.kts
+++ projects/toolkit/project.gradle.kts
@@ -1,6 +1,15 @@
+repositories {
+	maven(url = "https://maven.wispforest.io/") {
+		name = "WispForest"
+	}
+}
+
 dependencies {
 	"api"(project(":projects:pswg", configuration = "namedElements"))
 
 	"modImplementation"("com.formdev:flatlaf:2.6")
 	"include"("com.formdev:flatlaf:2.6")
+
+	"modImplementation"("io.wispforest:worldmesher:0.2.13+1.19")
+	"include"("io.wispforest:worldmesher:0.2.13+1.19")
 }
--- projects/toolkit/src/main/java/com/parzivail/pswgtk/ui/ToolkitHomeScreen.java
+++ projects/toolkit/src/main/java/com/parzivail/pswgtk/ui/ToolkitHomeScreen.java
@@ -4,14 +4,15 @@ import com.mojang.blaze3d.systems.RenderSystem;
 import com.parzivail.pswg.Resources;
 import com.parzivail.pswgtk.screen.JComponentScreen;
 import com.parzivail.pswgtk.swing.TextureBackedContentWrapper;
+import com.parzivail.pswgtk.world.ParametricBlockRenderView;
+import io.wispforest.worldmesher.WorldMesh;
+import net.minecraft.block.BlockState;
 import net.minecraft.block.Blocks;
 import net.minecraft.client.gui.screen.Screen;
-import net.minecraft.client.render.*;
-import net.minecraft.client.render.model.json.ModelTransformation;
+import net.minecraft.client.render.GameRenderer;
 import net.minecraft.client.util.math.MatrixStack;
-import net.minecraft.item.ItemStack;
-import net.minecraft.item.Items;
 import net.minecraft.text.Text;
+import net.minecraft.util.math.BlockPos;
 import net.minecraft.util.math.Quaternion;
 import net.minecraft.util.math.Vec2f;
 
@@ -26,8 +27,9 @@ public class ToolkitHomeScreen extends JComponentScreen implements MouseListener
 
 	private final JSplitPane root;
 	private final JPanel contentPanel;
-	private final JComboBox<ItemStack> comboBox;
-	private final JSlider slider;
+	private final JButton rebuildButton;
+
+	private final WorldMesh mesh;
 
 	public ToolkitHomeScreen(Screen parent)
 	{
@@ -35,20 +37,8 @@ public class ToolkitHomeScreen extends JComponentScreen implements MouseListener
 
 		var panel = new JPanel();
 
-		this.comboBox = new JComboBox<>();
-		this.comboBox.addItem(new ItemStack(Items.DIAMOND_SWORD));
-		this.comboBox.addItem(new ItemStack(Items.APPLE));
-		this.comboBox.addItem(new ItemStack(Items.SPYGLASS));
-		this.comboBox.addItem(new ItemStack(Blocks.TALL_GRASS));
-		this.comboBox.addItem(new ItemStack(Blocks.SCAFFOLDING));
-		this.comboBox.addItem(new ItemStack(Blocks.BIRCH_FENCE));
-		this.comboBox.addItem(new ItemStack(Blocks.DRAGON_HEAD));
-		this.comboBox.setSelectedIndex(0);
-
-		panel.add(this.comboBox);
-
-		this.slider = new JSlider(-180, 180, 0);
-		panel.add(this.slider);
+		panel.add(this.rebuildButton = new JButton("Rebuild"));
+		this.rebuildButton.addMouseListener(this);
 
 		this.root = new JSplitPane(JSplitPane.HORIZONTAL_SPLIT);
 		this.root.setLeftComponent(panel);
@@ -56,6 +46,21 @@ public class ToolkitHomeScreen extends JComponentScreen implements MouseListener
 		this.contentPanel = new JPanel();
 		this.contentPanel.setBackground(new Color(TextureBackedContentWrapper.MASK_COLOR));
 		this.root.setRightComponent(contentPanel);
+
+		mesh = new WorldMesh.Builder(new ParametricBlockRenderView(this::getBlockState), BlockPos.ORIGIN, new BlockPos(32, 32, 32))
+				.build();
+	}
+
+	private BlockState getBlockState(BlockPos pos)
+	{
+		var x = pos.getX() - 16;
+		var y = pos.getY() - 16;
+		var z = pos.getZ() - 16;
+		var R = 12;
+		var r = 6;
+		if (Math.pow(R - Math.sqrt(x * x + z * z), 2) + y * y <= r * r)
+			return Blocks.FIRE.getDefaultState();
+		return Blocks.AIR.getDefaultState();
 	}
 
 	@Override
@@ -77,6 +82,13 @@ public class ToolkitHomeScreen extends JComponentScreen implements MouseListener
 	@Override
 	public void mouseClicked(MouseEvent e)
 	{
+		if (e.getSource() == rebuildButton)
+		{
+			// TODO: if you render a set of blocks that use one render layer,
+			//  delete those blocks, add new blocks which use a different render
+			//  layer, and scheduleRebuild, the original blocks remain
+			mesh.scheduleRebuild();
+		}
 	}
 
 	@Override
@@ -104,6 +116,9 @@ public class ToolkitHomeScreen extends JComponentScreen implements MouseListener
 	{
 		assert this.client != null;
 
+		if (!mesh.canRender())
+			return;
+
 		RenderSystem.setShader(GameRenderer::getPositionTexColorShader);
 
 		var rsm = RenderSystem.getModelViewStack();
@@ -113,20 +128,27 @@ public class ToolkitHomeScreen extends JComponentScreen implements MouseListener
 
 		rsm.translate(contentCenter.x, contentCenter.y, 50);
 		rsm.scale(1, -1, 1);
-		rsm.scale(100, 100, 100);
+		var f = 6;
+		rsm.scale(f, f, f);
+		//		rsm.scale(100, 100, 100);
 		RenderSystem.applyModelViewMatrix();
 
-		var ir = this.client.getItemRenderer();
-		var stack = (ItemStack)comboBox.getSelectedItem();
+		//		var ir = this.client.getItemRenderer();
+		//		var stack = (ItemStack)comboBox.getSelectedItem();
 
 		var ms = new MatrixStack();
-		ms.multiply(new Quaternion(this.slider.getValue(), 0, 0, true));
+		ms.multiplyPositionMatrix(RenderSystem.getModelViewMatrix());
+		ms.multiply(new Quaternion(30, 0, 0, true));
 		ms.multiply(new Quaternion(0, (System.currentTimeMillis() % 36000) / 100f, 0, true));
 
-		VertexConsumerProvider.Immediate immediate = VertexConsumerProvider.immediate(Tessellator.getInstance().getBuffer());
-		var model = ir.getModel(stack, null, null, 0);
-		ir.renderItem(stack, ModelTransformation.Mode.NONE, false, ms, immediate, LightmapTextureManager.MAX_LIGHT_COORDINATE, OverlayTexture.DEFAULT_UV, model);
-		immediate.draw();
+		ms.translate(-16, -16, -16);
+
+		//		VertexConsumerProvider.Immediate immediate = VertexConsumerProvider.immediate(Tessellator.getInstance().getBuffer());
+		//		var model = ir.getModel(stack, null, null, 0);
+		//		ir.renderItem(stack, ModelTransformation.Mode.NONE, false, ms, immediate, LightmapTextureManager.MAX_LIGHT_COORDINATE, OverlayTexture.DEFAULT_UV, model);
+		//		immediate.draw();
+
+		this.mesh.render(ms);
 
 		rsm.pop();
 		RenderSystem.applyModelViewMatrix();
