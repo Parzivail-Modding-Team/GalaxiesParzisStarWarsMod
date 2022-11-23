4ee532f26a05a06a4a328602e917648517861cd1
parzivail <colby.newman2000@gmail.com>
2022-11-23 14:42:50 -0500

    add kenobi padawan model

--- projects/pswg/src/main/java/com/parzivail/pswg/BaseContent.java
+++ projects/pswg/src/main/java/com/parzivail/pswg/BaseContent.java
@@ -299,6 +299,7 @@ public class BaseContent implements PswgAddon, PswgClientAddon
 				new LightsaberDescriptor(Resources.id("anakin"), "Anakin Skywalker", Resources.id("anakin"), 0.62f, 1, 1),
 				new LightsaberDescriptor(Resources.id("ezra_padawan"), "Ezra Bridger", Resources.id("ezra_padawan"), 0.33f, 1, 1),
 				new LightsaberDescriptor(Resources.id("kenobi"), "Obi-Wan Kenobi", Resources.id("kenobi"), 0.62f, 1, 1),
+				new LightsaberDescriptor(Resources.id("kenobi_padawan"), "Padawan Obi-Wan Kenobi", Resources.id("kenobi_padawan"), 0.62f, 1, 1),
 				new LightsaberDescriptor(Resources.id("luke_rotj"), "Luke Skywalker", Resources.id("luke_rotj"), 0.33f, 1, 1),
 				new LightsaberDescriptor(Resources.id("qui_gon"), "Qui-Gon Jinn", Resources.id("qui_gon"), 0.33f, 1, 1),
 				new LightsaberDescriptor(Resources.id("vader"), "Darth Vader", Resources.id("vader"), 0, 1, 1)
