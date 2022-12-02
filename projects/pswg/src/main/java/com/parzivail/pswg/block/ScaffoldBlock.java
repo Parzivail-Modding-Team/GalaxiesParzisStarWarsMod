a3861dd5c53ba93fd147e7af7ecb7268969cb9f6
parzivail <colby.newman2000@gmail.com>
2022-12-02 17:22:35 -0500

    make scaffolding work like scaffolding

--- projects/pswg/src/main/java/com/parzivail/pswg/block/ScaffoldBlock.java
+++ projects/pswg/src/main/java/com/parzivail/pswg/block/ScaffoldBlock.java
@@ -1,14 +1,16 @@
 package com.parzivail.pswg.block;
 
-import com.parzivail.util.block.WaterloggableBlock;
 import net.minecraft.block.BlockState;
+import net.minecraft.block.ScaffoldingBlock;
 import net.minecraft.block.ShapeContext;
 import net.minecraft.util.math.BlockPos;
+import net.minecraft.util.math.Direction;
 import net.minecraft.util.shape.VoxelShape;
 import net.minecraft.util.shape.VoxelShapes;
 import net.minecraft.world.BlockView;
+import net.minecraft.world.WorldView;
 
-public class ScaffoldBlock extends WaterloggableBlock
+public class ScaffoldBlock extends ScaffoldingBlock
 {
 	private static final VoxelShape SHAPE = getShape();
 
@@ -23,9 +25,39 @@ public class ScaffoldBlock extends WaterloggableBlock
 		return SHAPE;
 	}
 
+	@Override
+	public boolean canPlaceAt(BlockState state, WorldView world, BlockPos pos)
+	{
+		return calculateOverriddenDistance(world, pos) < 7;
+	}
+
+	private int calculateOverriddenDistance(BlockView world, BlockPos pos)
+	{
+		var mutable = pos.mutableCopy().move(Direction.DOWN);
+		var blockState = world.getBlockState(mutable);
+		var i = 7;
+		if (blockState.isOf(this))
+			i = blockState.get(DISTANCE);
+		else if (blockState.isSideSolidFullSquare(world, mutable, Direction.UP))
+			return 0;
+
+		for (var direction : Direction.Type.HORIZONTAL)
+		{
+			var blockState2 = world.getBlockState(mutable.set(pos, direction));
+			if (blockState2.isOf(this))
+			{
+				i = Math.min(i, blockState2.get(DISTANCE) + 1);
+				if (i == 1)
+					break;
+			}
+		}
+
+		return i;
+	}
+
 	private static VoxelShape getShape()
 	{
-		VoxelShape shape = VoxelShapes.empty();
+		var shape = VoxelShapes.empty();
 		shape = VoxelShapes.union(shape, VoxelShapes.cuboid(0, 0, 0, 0.125, 1, 0.125));
 		shape = VoxelShapes.union(shape, VoxelShapes.cuboid(0, 0, 0.875, 0.125, 1, 1));
 		shape = VoxelShapes.union(shape, VoxelShapes.cuboid(0.875, 0, 0, 1, 1, 0.125));
