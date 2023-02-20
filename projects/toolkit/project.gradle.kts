01e7a0dacaaadca0b0584171cb43e12ab85de22b
parzivail <colby.newman2000@gmail.com>
2023-02-20 18:54:40 -0500

    remove flatlaf dep, update worldmesher, fix aurek entrypoint

--- projects/toolkit/project.gradle.kts
+++ projects/toolkit/project.gradle.kts
@@ -7,9 +7,6 @@ repositories {
 dependencies {
 	"api"(project(":projects:pswg", configuration = "namedElements"))
 
-	"modImplementation"("com.formdev:flatlaf:2.6")
-	"include"("com.formdev:flatlaf:2.6")
-
 	"modImplementation"("com.jetbrains.intellij.java:java-gui-forms-rt:222.4345.14")
 	"include"("com.jetbrains.intellij.java:java-gui-forms-rt:222.4345.14")
 
@@ -20,8 +17,8 @@ dependencies {
 	"modImplementation"("io.github.spair:imgui-java-natives-windows:1.86.6")
 	"include"("io.github.spair:imgui-java-natives-windows:1.86.6")
 
-	"modImplementation"("io.wispforest:worldmesher:0.2.14+1.19")
-	"include"("io.wispforest:worldmesher:0.2.14+1.19")
+	"modImplementation"("io.wispforest:worldmesher:0.2.14+1.19.3")
+	"include"("io.wispforest:worldmesher:0.2.14+1.19.3")
 	//	"modImplementation"(files("/home/cnewman/IdeaProjects/worldmesher/build/libs/worldmesher-0.2.13+build.11+1.19.jar"))
 	//	"include"(files("/home/cnewman/IdeaProjects/worldmesher/build/libs/worldmesher-0.2.13+build.11+1.19.jar"))
 }
