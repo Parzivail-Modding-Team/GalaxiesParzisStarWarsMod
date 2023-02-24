c57d40589890e334aaa7e55e81b8b6459df0a645
parzivail <colby.newman2000@gmail.com>
2023-02-23 19:35:04 -0500

    merge required and preset attachment builder parameters under certain conditions

--- projects/pswg/src/main/java/com/parzivail/pswg/BaseContent.java
+++ projects/pswg/src/main/java/com/parzivail/pswg/BaseContent.java
@@ -166,12 +166,9 @@ public class BaseContent implements PswgAddon, PswgClientAddon
 								             .attachment(2, "short_scope", BlasterAttachmentFunction.DEFAULT_SCOPE, BlasterAttachmentCategory.SCOPE, "short_scope", Resources.id("textures/item/model/blaster/ee3_rotj.png"))
 								             .attachment(3, "smooth_barrel", BlasterAttachmentFunction.INCREASE_RANGE, BlasterAttachmentCategory.BARREL, "smooth_barrel", Resources.id("textures/item/model/blaster/ee3_esb.png"))
 								             .attachment(3, "vane_barrel", BlasterAttachmentFunction.ALLOW_BURST, BlasterAttachmentCategory.BARREL, "vane_barrel", Resources.id("textures/item/model/blaster/ee3_rotj.png"))
-								             .required(1)
-								             .preset(1, "stock_wood")
-								             .required(2)
-								             .preset(2, "tall_scope")
-								             .required(3)
-								             .preset(3, "smooth_barrel")
+								             .required(1, "stock_wood")
+								             .required(2, "tall_scope")
+								             .required(3, "smooth_barrel")
 								             .build()),
 				new BlasterDescriptor(Resources.id("ca87"), BlasterArchetype.ION)
 						.firingBehavior(List.of(BlasterFiringMode.ION), BlasterWaterBehavior.NONE)
@@ -252,8 +249,7 @@ public class BaseContent implements PswgAddon, PswgClientAddon
 								             .attachment(1, "short_barrel", BlasterAttachmentFunction.INCREASE_DAMAGE, BlasterAttachmentCategory.BARREL, "short_barrel", null)
 								             .attachment(2, "scope", BlasterAttachmentFunction.DEFAULT_SCOPE, BlasterAttachmentCategory.SCOPE, "scope", null)
 								             .attachment(3, "repeater", BlasterAttachmentFunction.ALLOW_AUTO, BlasterAttachmentCategory.INTERNAL_ORDNANCE_CONFIG)
-								             .required(1)
-								             .preset(1, "long_barrel")
+								             .required(1, "long_barrel")
 								             .build())
 		);
 	}
