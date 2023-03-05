a885698061876f0ce49fa0fdffff00da3c7093b7
parzivail <colby.newman2000@gmail.com>
2023-03-05 00:18:16 -0500

    add example payload for rollbar api

--- projects/pswg/src/main/java/com/parzivail/errorman/ErrorManager.java
+++ projects/pswg/src/main/java/com/parzivail/errorman/ErrorManager.java
@@ -1,5 +1,6 @@
 package com.parzivail.errorman;
 
+import net.fabricmc.loader.api.FabricLoader;
 import net.minecraft.util.crash.CrashReport;
 import org.lwjgl.util.tinyfd.TinyFileDialogs;
 
@@ -7,8 +8,62 @@ import java.util.Arrays;
 
 public class ErrorManager
 {
+	/*
+	POST https://api.rollbar.com/api/1/item/
+	X-Rollbar-Access-Token: <client token>
+	Accept: application/json
+	Content-Type: application/json
+
+{
+	"data": {
+		"environment": "<Minecraft version>",
+		"platform": "client",
+		"code_version": "<PSWG version>",
+		"language": "java",
+		"request": {
+			"mods": {
+				"<modid>": "<version>",
+			}
+		},
+		"body": {
+			"trace_chain": [
+				{
+					"exception": {
+						"class": "Caused By",
+						"message": "Message"
+					},
+					"frames": [
+						{
+							"filename": "Pswg.java",
+							"lineno": 789,
+							"method": "load"
+						}
+					]
+				},
+				{
+					"exception": {
+						"class": "Outer Error",
+						"message": "Message"
+					},
+					"frames": [
+						{
+							"filename": "MinecraftClient2.java",
+							"lineno": 24,
+							"method": "start"
+						}
+					]
+				}
+			]
+		}
+	}
+}
+	 */
+
 	public static void onCrash(CrashReport report)
 	{
+		if (FabricLoader.getInstance().isDevelopmentEnvironment())
+			return;
+
 		var cause = report.getCause();
 		var atFault = false;
 
