c4d98b28311323760c9928f2ff2a0391d2440f43
parzivail <colby.newman2000@gmail.com>
2023-04-30 16:17:36 -0400

    fix pick block stack for mannequins

--- projects/pswg/src/main/java/com/parzivail/pswg/entity/MannequinEntity.java
+++ projects/pswg/src/main/java/com/parzivail/pswg/entity/MannequinEntity.java
@@ -1,7 +1,9 @@
 package com.parzivail.pswg.entity;
 
+import com.parzivail.pswg.container.SwgItems;
 import net.minecraft.entity.EntityType;
 import net.minecraft.entity.decoration.ArmorStandEntity;
+import net.minecraft.item.ItemStack;
 import net.minecraft.world.World;
 
 public class MannequinEntity extends ArmorStandEntity
@@ -14,5 +16,11 @@ public class MannequinEntity extends ArmorStandEntity
 	public MannequinEntity(World world, double x, double y, double z)
 	{
 		super(world, x, y, z);
+	}
+
+	@Override
+	public ItemStack getPickBlockStack()
+	{
+		return new ItemStack(SwgItems.Spawners.Mannequin);
 	}
 }
