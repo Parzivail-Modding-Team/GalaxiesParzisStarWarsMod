edf82be84c2e4a181d272c6efda6497477843742
parzivail <colby.newman2000@gmail.com>
2023-05-28 18:06:17 -0400

    fix test-addon event handler

--- projects/addon-test/src/main/java/com/example/addon/TestAddon.java
+++ projects/addon-test/src/main/java/com/example/addon/TestAddon.java
@@ -2,6 +2,7 @@ package com.example.addon;
 
 import com.parzivail.pswg.api.PswgAddon;
 import com.parzivail.pswg.api.PswgContent;
+import com.parzivail.pswg.api.RegisterResult;
 import com.parzivail.util.Lumberjack;
 import com.parzivail.util.math.ColorUtil;
 
@@ -24,6 +25,7 @@ public class TestAddon implements PswgAddon
 		PswgContent.LIGHTSABER_REGISTERED.register((id, descriptor) -> {
 			if (id.toString().equals("pswg:luke_rotj"))
 				descriptor.bladeColor = ColorUtil.packHsv(0.76f, 1, 1);
+			return RegisterResult.Pass;
 		});
 	}
 }
