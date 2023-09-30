a6903d9afe656e960d6630de0dc134eee951223f
kb1000 <kaeptmblaubaer1000@gmail.com>
2023-09-30 19:38:51 +0200

    Fix REI Move Ingredients

--- projects/pswg/src/main/java/com/parzivail/pswg/compat/rei/plugins/GalaxiesREIClientPlugin.java
+++ projects/pswg/src/main/java/com/parzivail/pswg/compat/rei/plugins/GalaxiesREIClientPlugin.java
@@ -7,11 +7,14 @@ import com.parzivail.pswg.compat.rei.displays.MoistureVaporatorDisplay;
 import com.parzivail.pswg.container.SwgBlocks;
 import com.parzivail.pswg.container.SwgRecipeType;
 import com.parzivail.pswg.recipe.VaporatorRecipe;
+import com.parzivail.pswg.screen.MoistureVaporatorScreenHandler;
 import me.shedaniel.math.Rectangle;
 import me.shedaniel.rei.api.client.plugins.REIClientPlugin;
 import me.shedaniel.rei.api.client.registry.category.CategoryRegistry;
 import me.shedaniel.rei.api.client.registry.display.DisplayRegistry;
 import me.shedaniel.rei.api.client.registry.screen.ScreenRegistry;
+import me.shedaniel.rei.api.client.registry.transfer.TransferHandlerRegistry;
+import me.shedaniel.rei.api.client.registry.transfer.simple.SimpleTransferHandler;
 import me.shedaniel.rei.api.common.util.EntryStacks;
 import net.minecraft.util.Identifier;
 
@@ -36,5 +39,12 @@ public class GalaxiesREIClientPlugin implements REIClientPlugin
 	public void registerScreens(ScreenRegistry registry)
 	{
 		registry.registerContainerClickArea(new Rectangle(61, 26 , 52, 32), MoistureVaporatorScreen.class, GalaxiesREICategories.MOISTURE_VAPORATOR);
+	}
+
+	@SuppressWarnings("UnstableApiUsage")
+	@Override
+	public void registerTransferHandlers(TransferHandlerRegistry registry)
+	{
+		registry.register(SimpleTransferHandler.create(MoistureVaporatorScreenHandler.class, GalaxiesREICategories.MOISTURE_VAPORATOR, new SimpleTransferHandler.IntRange(0, 1)));
 	}
 }
--- projects/pswg/src/main/java/com/parzivail/pswg/compat/rei/plugins/GalaxiesREIServerPlugin.java
+++ projects/pswg/src/main/java/com/parzivail/pswg/compat/rei/plugins/GalaxiesREIServerPlugin.java
@@ -1,12 +1,8 @@
 package com.parzivail.pswg.compat.rei.plugins;
 
 import com.parzivail.pswg.compat.rei.displays.MoistureVaporatorDisplay;
-import com.parzivail.pswg.screen.MoistureVaporatorScreenHandler;
 import me.shedaniel.rei.api.common.display.DisplaySerializerRegistry;
 import me.shedaniel.rei.api.common.plugins.REIServerPlugin;
-import me.shedaniel.rei.api.common.transfer.info.MenuInfoRegistry;
-import me.shedaniel.rei.api.common.transfer.info.simple.RecipeBookGridMenuInfo;
-import me.shedaniel.rei.api.common.transfer.info.simple.SimpleMenuInfoProvider;
 
 public class GalaxiesREIServerPlugin implements REIServerPlugin
 {
@@ -14,11 +10,5 @@ public class GalaxiesREIServerPlugin implements REIServerPlugin
 	public void registerDisplaySerializer(DisplaySerializerRegistry registry)
 	{
 		registry.register(GalaxiesREICategories.MOISTURE_VAPORATOR, MoistureVaporatorDisplay.serializer());
-	}
-
-	@Override
-	public void registerMenuInfo(MenuInfoRegistry registry)
-	{
-		registry.register(GalaxiesREICategories.MOISTURE_VAPORATOR, MoistureVaporatorScreenHandler.class, SimpleMenuInfoProvider.of(RecipeBookGridMenuInfo::new));
 	}
 }
