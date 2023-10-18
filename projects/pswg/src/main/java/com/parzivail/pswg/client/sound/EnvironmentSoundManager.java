89e0decbb809f2fa0aad294497fd633a4286ebd3
parzivail <colby.newman2000@gmail.com>
2023-10-17 21:04:31 -0400

    vastly improved blaster bolt flyby sounds

--- projects/pswg/src/main/java/com/parzivail/pswg/client/sound/EnvironmentSoundManager.java
+++ projects/pswg/src/main/java/com/parzivail/pswg/client/sound/EnvironmentSoundManager.java
@@ -1,9 +1,9 @@
 package com.parzivail.pswg.client.sound;
 
+import com.parzivail.pswg.container.SwgSounds;
 import com.parzivail.pswg.entity.BlasterBoltEntity;
 import net.minecraft.client.MinecraftClient;
 import net.minecraft.sound.SoundCategory;
-import net.minecraft.sound.SoundEvents;
 import net.minecraft.util.math.Box;
 
 public class EnvironmentSoundManager
@@ -15,7 +15,7 @@ public class EnvironmentSoundManager
 
 		var pos = mc.player.getEyePos();
 
-		var box = Box.of(mc.player.getPos(), 16, 16, 16);
+		var box = Box.of(pos, 16, 16, 16);
 		for (var entity : mc.world.getEntitiesByClass(BlasterBoltEntity.class, box, e -> true))
 		{
 			var entityDirection = entity.getVelocity();
@@ -28,7 +28,7 @@ public class EnvironmentSoundManager
 			var oldPlayerDot = entityDirection.dotProduct(oldPlayerDelta);
 			var newPlayerDot = entityDirection.dotProduct(newPlayerDelta);
 
-			if (oldPlayerDot >= 0 && newPlayerDot < 0)
+			if (oldPlayerDot <= 0 && newPlayerDot > 0)
 			{
 				// Entity transitioned from moving towards the
 				// player (i.e. pos-player vector and direction
@@ -36,7 +36,8 @@ public class EnvironmentSoundManager
 				// the player (i.e. vectors point opposite
 				// directions)
 
-				mc.world.playSound(newEntityPos.x, newEntityPos.y, newEntityPos.z, SoundEvents.ENTITY_GHAST_SCREAM, SoundCategory.PLAYERS, 1, 1, true);
+				var pitch = 0.7f + mc.world.random.nextFloat() * 0.6f;
+				mc.world.playSound(newEntityPos.x, newEntityPos.y, newEntityPos.z, SwgSounds.Blaster.FLYBY, SoundCategory.PLAYERS, 1, pitch, true);
 			}
 		}
 	}
