1eea8ff096304b0a4eda821f2fa9a52c6f336839
parzivail <colby.newman2000@gmail.com>
2023-10-18 22:07:21 -0400

    enable freetype font rendering in aurek

--- projects/toolkit/project.gradle.kts
+++ projects/toolkit/project.gradle.kts
@@ -17,8 +17,8 @@ dependencies {
 	"include"("io.github.spair:imgui-java-binding:1.86.11")
 	"modImplementation"("io.github.spair:imgui-java-lwjgl3:1.86.11")
 	"include"("io.github.spair:imgui-java-lwjgl3:1.86.11")
-	"modImplementation"("io.github.spair:imgui-java-natives-windows:1.86.11")
-	"include"("io.github.spair:imgui-java-natives-windows:1.86.11")
+	"modImplementation"("io.github.spair:imgui-java-natives-windows-ft:1.86.11")
+	"include"("io.github.spair:imgui-java-natives-windows-ft:1.86.11")
 
 	"modImplementation"("io.wispforest:worldmesher:0.4.2+1.20")
 	"include"("io.wispforest:worldmesher:0.4.2+1.20")
