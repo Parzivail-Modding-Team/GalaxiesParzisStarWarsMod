bd5f3f20e606810886eb8f92a6042823b85ede35
DeltaHelios <stefan.v.iancu@gmail.com>
2024-01-24 13:40:59 +0200

    Placeble thermal detonator proper model and hitbox

--- projects/pswg/src/main/java/com/parzivail/pswg/block/ThermalDetonatorBlock.java
+++ projects/pswg/src/main/java/com/parzivail/pswg/block/ThermalDetonatorBlock.java
@@ -1,27 +1,51 @@
 package com.parzivail.pswg.block;
 
+import com.google.common.collect.ImmutableMap;
 import com.parzivail.pswg.container.SwgEntities;
 import com.parzivail.pswg.entity.BlasterBoltEntity;
 import com.parzivail.pswg.entity.ThermalDetonatorEntity;
+import com.parzivail.util.block.VoxelShapeUtil;
+import com.parzivail.util.block.rotating.WaterloggableRotatingBlock;
 import net.minecraft.block.Block;
 import net.minecraft.block.BlockState;
 import net.minecraft.block.Blocks;
+import net.minecraft.block.ShapeContext;
 import net.minecraft.entity.Entity;
 import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.entity.projectile.ArrowEntity;
 import net.minecraft.entity.projectile.ProjectileEntity;
 import net.minecraft.text.Text;
+import net.minecraft.util.function.BooleanBiFunction;
 import net.minecraft.util.hit.BlockHitResult;
 import net.minecraft.util.math.BlockPos;
+import net.minecraft.util.shape.VoxelShape;
+import net.minecraft.util.shape.VoxelShapes;
+import net.minecraft.world.BlockView;
 import net.minecraft.world.World;
 import net.minecraft.world.explosion.Explosion;
 
-public class ThermalDetonatorBlock extends Block
+import java.util.function.Function;
+
+public class ThermalDetonatorBlock extends WaterloggableRotatingBlock
 {
 
 	public ThermalDetonatorBlock(Settings settings)
 	{
 		super(settings);
+	}
+
+	@Override
+	public VoxelShape getOutlineShape(BlockState state, BlockView world, BlockPos pos, ShapeContext context)
+	{
+		var shape = makeShape();
+		return VoxelShapeUtil.rotateToFace(shape, state.get(FACING));
+	}
+
+	@Override
+	protected ImmutableMap<BlockState, VoxelShape> getShapesForStates(Function<BlockState, VoxelShape> stateToShape)
+	{
+
+		return super.getShapesForStates(stateToShape);
 	}
 
 	@Override
@@ -35,10 +59,6 @@ public class ThermalDetonatorBlock extends Block
 			tde.setLife(0);
 			world.spawnEntity(tde);
 		}
-		if (entity instanceof PlayerEntity player)
-		{
-			player.sendMessage(Text.of("IHATEYOU"), true);
-		}
 		super.onEntityCollision(state, world, pos, entity);
 	}
 
@@ -50,6 +70,15 @@ public class ThermalDetonatorBlock extends Block
 			explode(world, hit.getBlockPos());
 		}
 		super.onProjectileHit(world, state, hit, projectile);
+	}
+
+	public VoxelShape makeShape()
+	{
+		VoxelShape shape = VoxelShapes.empty();
+		shape = VoxelShapes.combineAndSimplify(shape, VoxelShapes.cuboid(0.40625, 0, 0.40625, 0.59375, 0.1875, 0.59375), BooleanBiFunction.OR);
+		shape = VoxelShapes.combineAndSimplify(shape, VoxelShapes.cuboid(0.46875, 0.15625, 0.421875, 0.53125, 0.21875, 0.546875), BooleanBiFunction.OR);
+
+		return shape;
 	}
 
 	@Override
