925b1f94bf91250add6214f6e120f32615b112fb
parzivail <colby.newman2000@gmail.com>
2024-12-05 18:34:44 -0500

    itemstack GUI render events

---
+++ projects/pswg_core/src/client/java/dev/pswg/mixin/client/events/DrawContextMixin.java
@@ -0,0 +1,26 @@
+package dev.pswg.mixin.client.events;
+
+import dev.pswg.events.ItemRenderEvents;
+import net.minecraft.client.font.TextRenderer;
+import net.minecraft.client.gui.DrawContext;
+import net.minecraft.item.ItemStack;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+
+@Mixin(DrawContext.class)
+public abstract class DrawContextMixin
+{
+	@Inject(method = "drawStackOverlay(Lnet/minecraft/client/font/TextRenderer;Lnet/minecraft/item/ItemStack;IILjava/lang/String;)V", at = @At(value = "INVOKE", target = "Lnet/minecraft/client/gui/DrawContext;drawCooldownProgress(Lnet/minecraft/item/ItemStack;II)V", shift = At.Shift.AFTER))
+	public void drawStackOverlay(TextRenderer textRenderer, ItemStack stack, int x, int y, String stackCountText, CallbackInfo ci)
+	{
+		var self = (DrawContext)(Object)this;
+
+		var matrix = self.getMatrices();
+
+		matrix.push();
+		ItemRenderEvents.STACK.invoker().render(self, textRenderer, stack, x, y);
+		matrix.pop();
+	}
+}
