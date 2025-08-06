c328e0417efb9154a68657d8d724bf7bdfb1fa75
DeltaHelios <stefan.v.iancu@gmail.com>
2025-08-06 16:12:36 +0300

    Small Impact Grenade model back-end adjustment

--- projects/pswg_gadgets/src/client/java/dev/pswg/models/ImpactGrenadeModel.java
+++ projects/pswg_gadgets/src/client/java/dev/pswg/models/ImpactGrenadeModel.java
@@ -20,10 +20,13 @@ public class ImpactGrenadeModel extends EntityModel<GrenadeRenderState>
 		modelPartData.addChild("body", ModelPartBuilder.create().uv(0, 0).cuboid(-1.5F, -3.0F, -1.5F, 3.0F, 3.0F, 3.0F, new Dilation(0.0F)), ModelTransform.of(0F, 0F, 0F, 0, 0, (float)Math.PI));
 		modelPartData.addChild("middle", ModelPartBuilder.create().uv(0, 9).cuboid(-1.0F, -4.0F, -1.0F, 2.0F, 1.0F, 2.0F, new Dilation(0.0F)), ModelTransform.of(0F, 0F, 0F, 0, 0, (float)Math.PI));
 		modelPartData.addChild("top", ModelPartBuilder.create().uv(6, 6).cuboid(-0.5F, -4.5F, -0.5F, 1.0F, 1.0F, 1.0F, new Dilation(0.0F)), ModelTransform.of(0F, 0F, 0F, 0, 0, (float)Math.PI));
-		modelPartData.addChild("pin_p1", ModelPartBuilder.create().uv(6, 13).cuboid(1F, -5F, -1.25F, 0.0F, 1.0F, 2.0F, new Dilation(0.025F)), ModelTransform.of(0F, 0.25F, 0, 0.1745F, 0, (float)Math.PI));
-		modelPartData.addChild("pin_p2", ModelPartBuilder.create().uv(6, 13).cuboid(-1.0F, -5F, -1.25F, 0.0F, 1.0F, 2.0F, new Dilation(0.025F)), ModelTransform.of(0F, 0.25F, 0, 0.1745F, 0, (float)Math.PI));
-		modelPartData.addChild("pin_p3", ModelPartBuilder.create().uv(2, 15).cuboid(-1.0F, -5.F, -1.25F, 2.0F, 1.0F, 0.0F, new Dilation(0.026F)), ModelTransform.of(0F, 0.25F, 0, 0.1745F, 0, (float)Math.PI));
-		modelPartData.addChild("pin_p4", ModelPartBuilder.create().uv(12, 15).cuboid(-1.0F, -5.F, 0.75F, 2.0F, 1.0F, 0.0F, new Dilation(0.026F)), ModelTransform.of(0F, 0.25F, 0, 0.1745F, 0, (float)Math.PI));
+		modelPartData.addChild("pin", ModelPartBuilder.create()
+		                                              .uv(6, 13).cuboid(1F, -5F, -1.25F, 0.0F, 1.0F, 2.0F, new Dilation(0.025F))
+		                                              .uv(6, 13).cuboid(-1.0F, -5F, -1.25F, 0.0F, 1.0F, 2.0F, new Dilation(0.025F))
+		                                              .uv(2, 15).cuboid(-1.0F, -5.F, -1.25F, 2.0F, 1.0F, 0.0F, new Dilation(0.026F))
+		                                              .uv(12, 15).cuboid(-1.0F, -5.F, 0.75F, 2.0F, 1.0F, 0.0F, new Dilation(0.026F)),
+		                       ModelTransform.of(0F, 0.25F, 0, 0.1745F, 0, (float)Math.PI));
+
 
 		return TexturedModelData.of(modelData, 16, 16);
 	}
