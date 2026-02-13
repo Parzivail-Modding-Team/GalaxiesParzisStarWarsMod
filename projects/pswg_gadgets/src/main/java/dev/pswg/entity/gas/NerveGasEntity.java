3684377f1a1844dea7d6731da0d5b979bc901f6a
DeltaHelios <stefan.v.iancu@gmail.com>
2026-02-13 20:10:00 +0200

    bugfix: fix intoxicated "blinking" effect caused by nerve gas

--- projects/pswg_gadgets/src/main/java/dev/pswg/entity/gas/NerveGasEntity.java
+++ projects/pswg_gadgets/src/main/java/dev/pswg/entity/gas/NerveGasEntity.java
@@ -1,8 +1,8 @@
 package dev.pswg.entity.gas;
 
+import dev.pswg.Gadgets;
 import dev.pswg.container.GadgetsParticleTypes;
 import dev.pswg.container.entity.GadgetsEffects;
-import dev.pswg.world.TickConstants;
 import net.minecraft.entity.EntityType;
 import net.minecraft.entity.LivingEntity;
 import net.minecraft.entity.effect.StatusEffectInstance;
@@ -58,10 +58,13 @@ public class NerveGasEntity extends GasEntity
 					if (livingEntity.hasStatusEffect(GadgetsEffects.INTOXICATED))
 					{
 						if (livingEntity.getStatusEffect(GadgetsEffects.INTOXICATED).getAmplifier() != amplifier)
-							livingEntity.setStatusEffect(new StatusEffectInstance(GadgetsEffects.INTOXICATED, TickConstants.ONE_DAY * 100, amplifier, false, false, true), this);
+						{
+							livingEntity.setStatusEffect(new StatusEffectInstance(GadgetsEffects.INTOXICATED, 100, amplifier, false, false, true), this);
+							Gadgets.LOGGER.info("test");
+						}
 					}
 					else
-						livingEntity.addStatusEffect(new StatusEffectInstance(GadgetsEffects.INTOXICATED, TickConstants.ONE_DAY * 100, amplifier, false, false, true), this);
+						livingEntity.addStatusEffect(new StatusEffectInstance(GadgetsEffects.INTOXICATED, 100, amplifier, false, false, true), this);
 				}
 			if (livingEntity.isDead())
 				remove.add(livingEntity);
