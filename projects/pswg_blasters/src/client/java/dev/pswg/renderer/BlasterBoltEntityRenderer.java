d1dff31935f89d47070062c312ec120f701a9a95
parzivail <colby.newman2000@gmail.com>
2026-04-11 15:41:24 -0400

    develop: put blaster bolt texture back

--- projects/pswg_blasters/src/client/java/dev/pswg/renderer/BlasterBoltEntityRenderer.java
+++ projects/pswg_blasters/src/client/java/dev/pswg/renderer/BlasterBoltEntityRenderer.java
@@ -50,7 +50,7 @@ public class BlasterBoltEntityRenderer extends EntityRenderer<BlasterBoltEntity,
 		public float yaw;
 	}
 
-	public static final Identifier TEXTURE = Identifier.parse("ptex.minecraft.sampler.tint:0xffff0000/textures/entity/projectiles/arrow.png");
+	public static final Identifier TEXTURE = Identifier.withDefaultNamespace("textures/entity/projectiles/arrow.png");
 	private final Model model;
 
 	public BlasterBoltEntityRenderer(EntityRendererProvider.Context context)
@@ -69,7 +69,7 @@ public class BlasterBoltEntityRenderer extends EntityRenderer<BlasterBoltEntity,
 		matrixStack.translate(0.2f, 0, 0);
 
 		this.model.setupAnim(state);
-		queue.submitModel(this.model, state, matrixStack, PtexTextures.resolve(TEXTURE), state.lightCoords, OverlayTexture.NO_OVERLAY, state.outlineColor, null);
+		queue.submitModel(this.model, state, matrixStack, TEXTURE, state.lightCoords, OverlayTexture.NO_OVERLAY, state.outlineColor, null);
 
 		matrixStack.popPose();
 	}
